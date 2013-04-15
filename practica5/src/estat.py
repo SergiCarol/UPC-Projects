75c5cfe881d68e4f8af6ff4bfd510baa05a4bdd4
david.martin.serrano <david.martin.serrano@6045123d-4387-4ba5-964e-f94074b1feba>
2013-04-15 07:57:34 +0000

    Vamos a ponerlo serio

--- practica5/src/estat.py
+++ practica5/src/estat.py
@@ -1,1 +1,77 @@
 # -*- encoding:utf-8 -*-
+
+"""
+Ahora se escribirá el modulo estado. Este modulo es muy util para
+comprobar en que estado se encuentra un nodo
+"""
+
+class Estat(object):
+    """
+    >>> e0 = Estat()
+    >>> e0.undef()
+    True
+    >>> e1 = Estat(1)
+    >>> e0 = Estat(0)
+    >>> e1 & e0
+    Estat(0)
+    >>> e0 & e0
+    Estat(0)
+    >>> e1 & e1
+    Estat(1)
+    >>> e1 | e0
+    Estat(1)
+    >>> e1 | e1
+    Estat(1)
+    
+    """
+    
+    def __init__(self, e = -1):
+        """
+        >>> p = Estat()
+        >>> p._e
+        -1
+        >>> t = Estat(3)
+        >>> t._e
+        -1
+        >>> t = Estat(0)
+        >>> t._e
+        0
+        """
+        if e not in (-1,0,1):
+            e = -1
+        self._e = e
+
+    def undef(self):
+        """
+        >>> p = Estat()
+        >>> a = p.undef()
+        >>> print a
+        False
+        >>> p._e = 0
+        >>> a = p.undef()
+        >>> print a
+        True
+        """
+        return self._e == -1
+
+    def __and__(self, altre):
+        """
+        
+        """
+        if self.undef() or altre.undef():
+            return Estat()
+        return Estat(self._e * altre._e)
+            
+"""
+    def __nonzero__(self):
+        return False
+
+    def __repr__(self):
+        return False
+
+
+
+    def __or__(self):
+        return False
+""" 
+        
