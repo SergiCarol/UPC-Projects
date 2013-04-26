1ef497c49e9df1ea116f6b1e15bd026971bfc632
sergi.carol <sergi.carol@6045123d-4387-4ba5-964e-f94074b1feba>
2013-04-26 07:16:15 +0000

    actualitzat

--- practica5/src/supervisor.py
+++ practica5/src/supervisor.py
@@ -1,19 +1,26 @@
-#-*- encoding:utf-8 -*-
-class Supervisor (Object):
+# -*- encoding:utf-8 -*-
+from node import *
+class Supervisor (object):
     def __int__(self):
         """
         Crea un supervisor buit 
         """
         self._nodes=[]
         self._triports=[]
+        self._changed=False
     def add_node(self,n):
         """
         Afegeix un node a supervisor
         """
+        
         self._nodes+=[n]
-    
+        n.set_supervisor(self)
+        n.self_supervisor(self)
     def add_triport(self,t):
         """
         Afegeix un triport al supervisor
         """
         self._triports+=[t]
+    
+    def notify_change(self):
+        self.changed=True
