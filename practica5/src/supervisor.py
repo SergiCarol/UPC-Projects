826760e64643ba32abec33ea4787c81a0123c666
sergi.carol <sergi.carol@6045123d-4387-4ba5-964e-f94074b1feba>
2013-04-26 06:37:25 +0000

    actualitzat

--- practica5/src/supervisor.py
+++ practica5/src/supervisor.py
@@ -1,1 +1,19 @@
 #-*- encoding:utf-8 -*-
+class Supervisor (Object):
+    def __int__(self):
+        """
+        Crea un supervisor buit 
+        """
+        self._nodes=[]
+        self._triports=[]
+    def add_node(self,n):
+        """
+        Afegeix un node a supervisor
+        """
+        self._nodes+=[n]
+    
+    def add_triport(self,t):
+        """
+        Afegeix un triport al supervisor
+        """
+        self._triports+=[t]
