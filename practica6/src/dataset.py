14fc06c8bd9c83226fc0a1d84aff0bd0816666b3
david.martin.serrano <david.martin.serrano@6045123d-4387-4ba5-964e-f94074b1feba>
2013-04-29 07:33:57 +0000

    Primeros pasos

---
+++ practica6/src/dataset.py
@@ -0,0 +1,37 @@
+import datetime
+
+
+class Dataset(object):
+
+	def __init__(self,name=''):
+		self._name = name
+		self._ds= []
+
+	def __len__(self):
+		return len(self._ds)
+
+	def __str__(self):
+		pass
+
+	def add(self,t,v):
+		self._ds += [(t,v)]
+
+	def time_vector(self):
+		temp = []
+		for x in self._ds:
+			a = self._ds[0[0]].hour()
+
+	def value_vector(self):
+		pass
+
+	def decimate(self,k=10):
+		pass
+
+	def moving average(self,k=50):
+		pass
+
+	def concat(self,ds2):
+		pass
+
+	def transform(self,a=1.0,b=0.0):
+		pass
