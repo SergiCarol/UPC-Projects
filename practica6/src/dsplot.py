3155ac7933508adcea647d2a8ebbc6501d64ef3b
sergi.carol <sergi.carol@6045123d-4387-4ba5-964e-f94074b1feba>
2013-05-10 08:42:08 +0000

    sbplot arreglat(crec que funciona)i acabat el def fetch del datafetcher

--- practica6/src/dsplot.py
+++ practica6/src/dsplot.py
@@ -1,6 +1,5 @@
 # -*- encoding:utf-8 -*-
 
-from pylab import figure
 import matplotlib.pyplot as plot
 import matplotlib.dates as dates
 from dataset import DataSet
@@ -11,7 +10,7 @@ class DataSetPlot(object):
 	def __init__(self):	
 
 		self.p = plot.plot([], [])
-		self.a = plot.axis([1, 7, 18, 30])
+		self.a = plot.axis([, , , ])
 		self.l = plot.legend([''])
 		self.count = 0
 		self.plt.grid(True)
@@ -19,22 +18,18 @@ class DataSetPlot(object):
 
 	def plot(self,d):
 		d=DataSet()
-		self.p = ([d.time_vector()], [d.value_vector()], color[self.count])
+		self.p = ([d.time_vector()], [d.value_vector()])
 		self.l = ([d._name()])		
-		color = ['r', 'g', 'y', 'b']
+	
 		self.count += 1
 		if self.count == 4:
 			self.count == 0
 
-	
+		self.a= ([self.hour,self.dateformat])
 
     def show(self):
 
-        ax = f.add_subplot(111)
-        for e in ax.get_xticklabels():
-            e.set_rotation(60)
-            ax.xaxis.set_major_formatter(dat.DateFormatter('%d/%m %H:%M'))
-            f.autofmt_xdate()
+	   
             self.p.show()
 
 
