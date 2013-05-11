4068eaf2e622fcffcd3fea867db82f488f31d1a2
sergi.carol <Sepharon@users.noreply.github.com>
2013-05-11 16:46:39 +0000

    Arreglant el explora, afegit explota

--- practica6/src/datafetcher.py
+++ practica6/src/datafetcher.py
@@ -2,10 +2,11 @@
 import urllib2
 from dataset import Dataset
 from datetime import *
+import csv
 class DataSetFetcher(object):
     def __init__(self,url='http://localhost:8000'):
         self.url=url
-    ±
+    
     def fetch(self,dia,sensor=0):
         Data=Dataset("Sensor"+str(sensor))
         dia_temps="dades_"+dia.strftime("%y")+"_"+dia.strftime("%m")+"_"+dia.strftime('%d')
@@ -14,7 +15,7 @@ class DataSetFetcher(object):
             with open(pag, 'rb') as f:
                 reader = csv.reader(f)
                 for row in reader:
-                    if row[1]=sensor:
+                    if row[1]==sensor:
                         Data.add(row[0],row[2])
                         
         except: 
@@ -24,12 +25,12 @@ class DataSetFetcher(object):
 	
     def fetch_interval(self,from_day,to_day,sensor=0):
         Data=Dataset("Sensor"+str(sensor))
-        #Mirar aquet troç, segurament és te que canviar
+        #Mirar aquet tros, segurament es te que canviar
 	d = date(from_day)
         d2 = date(to_day)
         while d < d2:
             self.fetch(d,sensor)
-            d= d+ datedela(days=1)
+            d= d+ timedelta(days=1)
         
 
 
