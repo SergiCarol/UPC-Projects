795010adb074fd35108c255be53246a961225032
david.martin.serrano <david.martin.serrano@6045123d-4387-4ba5-964e-f94074b1feba>
2013-05-12 17:19:09 +0000

    Actualizado Sphinx

--- practica6/src/sensortransf.py
+++ practica6/src/sensortransf.py
@@ -1,12 +1,5 @@
 # -*- encoding:utf-8 -*-
 
-"""
-Aquest modul solament exporta una funció capaç d'aplicar la transformació escaient a un DataSet per tal d'obtenir els valors de les temperatures.
-
-S'ha de tenir en compte que les dades que es llegeixen dels sensors no son pròpiament temperatures. Cal aplicar les transformacions lineals que allí s'expliquen, diferents per a cada sensor, per tal d'obtenir-ne les temperatures.
-"""
-
-
 def normalize(d,s=0):
     """
     La funció normalize(d,s=0) aplica la transformació escaient per al sensor s sobre el DataSet d.
