ab955f8f97ddd3c1f8b2e3da3d092f7286032f70
david.martin.serrano <david.martin.serrano@6045123d-4387-4ba5-964e-f94074b1feba>
2013-05-12 16:50:48 +0000

    Que no mireis el proyecto, coño ya

--- practica6/src/datafetcher.py
+++ practica6/src/datafetcher.py
@@ -1,13 +1,24 @@
-# -*- encoding_utf-8 -*-
+# -*- encoding:utf-8 -*-
+
 from urllib2 import *
 from dataset import Dataset
 from datetime import *
 import csv
+
 class DataSetFetcher(object):
+    """
+    La classe *datasetfetcher* serà l'encarregada de buscar en internet les dades del sensor que es vulgui trobar.
+    """
     def __init__(self,url='http://localhost:8000'):
+	"""
+	Inicialitza els objectes de la classe. Cal indicar el servidor d’on cal obtenir els fitxers de dades.
+	"""
         self.url=url
     
     def fetch(self,dia,sensor=0):
+	"""
+	El mètode retorna un DataSet de nom *SensorX*, essent X el numero de sensor, que conte les observacions del sensor sensor corresponents al dia dia. dia es un objecte de classe date. Si no es poden obtenir les dades aixeca l'excepció *UnknownDataSetException*.
+	"""
         Dat=Dataset("Sensor "+str(sensor))
         dia_temps="/dades_"+dia.strftime("%y")+"_"+dia.strftime("%m")+"_"+dia.strftime('%d')
         pag=urlopen(self.url+dia_temps)
@@ -26,6 +37,9 @@ class DataSetFetcher(object):
 
 	
     def fetch_interval(self,from_day,to_day,sensor=0):
+	"""
+	El mètode retorna un DataSet de nom *SensorX*, essent X el numero de sensor, que conte les observacions del sensor sensor corresponents als dies que van de from day a to day incloent el primer i excloent el darrer. from day i to day son objectes de la classe date. Si no es poden obtenir les dades aixeca l'excepció *UnknownDataSetException*.
+	"""
         Data=Dataset("Sensor"+str(sensor))
         while from_day < to_day:
             self.fetch(d,sensor)
@@ -36,4 +50,7 @@ class DataSetFetcher(object):
 
 
 class UnknownDataSetException (Exception):
+    """
+    Aquest error s'aixecarà en alguns casos de les funcions *fetch_interval* i *fetch*.
+    """
     pass
