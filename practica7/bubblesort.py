5711643d792ee01620056a894bbea8bd84d1c318
david.martin.serrano <david.martin.serrano@6045123d-4387-4ba5-964e-f94074b1feba>
2013-05-13 06:46:47 +0000

    :)

--- practica7/bubblesort.py
+++ practica7/bubblesort.py
@@ -1,11 +1,17 @@
 def ordenar(a):
 	for x in range(len(a)):
-		if a[x] > a[x+1]:
-			aux = a[x]
-			a[x] = a[x+1]
-			a[x+1] = aux
-			ordenar(a)
-		else:
+		try:
+			if a[x] > a[x+1]:
+				aux = a[x]
+				a[x] = a[x+1]
+				a[x+1] = aux
+				ordenar(a)
+			else:
+				pass
+		except:
 			pass
+	return a
 			
-			
+z = [7,6,8,3,90,1]
+ordenado = ordenar(z)
+print ordenado
