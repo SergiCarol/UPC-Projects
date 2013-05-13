891836dbdf34d3a367b87690cb11616c921aa85b
sergi.carol <sergi.carol@6045123d-4387-4ba5-964e-f94074b1feba>
2013-05-13 07:36:11 +0000

    Se tiene que hacer cosas

--- practica7/eines.py
+++ practica7/eines.py
@@ -1,14 +1,26 @@
 # -*- encoding:utf-8 -*-
-print "Adios"
 import random 
+import time
 def eines(n):
+    clk_reset()
     i=0
     a=[]
     while n>i:
         ran=random.randint(0,(2*n))
         a+=[ran]
             
-        i+=1    
+        i+=1 
     return a
-print eines(20)
 
+def clk_reset():
+    start=time.clock()
+    return start
+def clk_counter():
+    a=clk_reset()
+    for a in range(1000):
+        l= [2] * 20000
+        x=0
+        for i in l:
+            x+= i
+    mesura= time.rell() -a
+    print' {0:8.5f} ms'.format(mesura)
