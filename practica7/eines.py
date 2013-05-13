e69f63fad17a5b4514d9305a707eecc2f1d716a8
sergi.carol <Sepharon@users.noreply.github.com>
2013-05-13 07:16:01 +0000

    eines afegit

---
+++ practica7/eines.py
@@ -0,0 +1,13 @@
+# -*- encoding:utf-8 -*-
+import random 
+def eines(n):
+    i=0
+    a=[]
+    while n>i:
+        ran=random.randint(0,(2*n))
+        a+=[ran]
+            
+        i+=1    
+    return a
+print eines(20)
+
