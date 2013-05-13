ab7a6e40370af5af3368e9103c438ee658611bbe
david.martin.serrano <david.martin.serrano@6045123d-4387-4ba5-964e-f94074b1feba>
2013-05-13 07:15:01 +0000

    Sacatun

--- practica7/bubblesort.py
+++ practica7/bubblesort.py
@@ -1,6 +1,22 @@
 # -*- encoding:utf-8 -*-
 
 def ordenar(a):
+	"""
+	La funció *ordenar* será l'encarregada de rebre una llista de numeros, la cual retornará ordenada. S'ha de tenir en compte que aquesta funció té recursivitat.
+
+	>>> a = [1,2,3,4,5,6]
+	>>> s = ordenar(a)
+	>>> print s
+	[1, 2, 3, 4, 5, 6]
+	>>> b = [9,8,7,6,5,4]
+	>>> s = ordenar(b)
+	>>> print s
+	[4, 5, 6, 7, 8, 9]
+	>>> c = [23,10,47,-13,89]
+	>>> s = ordenar(c)
+	>>> print s
+	[-13, 10, 23, 47, 89]
+	"""
 	for x in range(len(a)):
 		try:
 			if a[x] > a[x+1]:
@@ -18,5 +34,5 @@ def ordenar(a):
 z = [7,6,8,3,90,1]
 print "La llista sense ordenar es la següent: " + str(z)
 ordenado = ordenar(z)
-print "El programa ja ha acabat d'ordenar, la llista és al següent: " + str(ordenado)
+print "El programa ja ha acabat d'ordenar, la llista és la següent: " + str(ordenado)
 
