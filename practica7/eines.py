1fffa623fa40a9f2db150b571b66f11a3eb90dfc
sergi.carol <sergi.carol@6045123d-4387-4ba5-964e-f94074b1feba>
2013-05-18 15:19:57 +0000

    Eines llest

--- practica7/eines.py
+++ practica7/eines.py
@@ -1,26 +1,25 @@
 # -*- encoding:utf-8 -*-
+import time
 import random 
 import time
 def eines(n):
-    clk_reset()
+    reset=clk_reset()
     i=0
     a=[]
     while n>i:
         ran=random.randint(0,(2*n))
         a+=[ran]
             
-        i+=1 
+        i+=1
+    clk_counter(reset)
     return a
 
 def clk_reset():
-    start=time.clock()
+    start=time.time()
     return start
-def clk_counter():
-    a=clk_reset()
-    for a in range(1000):
-        l= [2] * 20000
-        x=0
-        for i in l:
-            x+= i
-    mesura= time.rell() -a
-    print' {0:8.5f} ms'.format(mesura)
+def clk_counter(r):
+    temps_total=time.time()-r
+    print temps_total
+    return temps_total
+n=20 
+print eines(n)
