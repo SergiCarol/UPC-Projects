255c58b26011ba8693ba74fe2aa4982faf49f548
enric.lenard <enric.lenard@6045123d-4387-4ba5-964e-f94074b1feba>
2014-03-20 11:11:21 +0000

    Modifiquem semphore.c

--- PBN/P4/semaphore.c
+++ PBN/P4/semaphore.c
@@ -1,1 +1,20 @@
 #include<stdio.h>
+
+void semaphore_init(void){
+  semaphore_state_t state;
+  state = SempahoreOff;
+}
+void semaphore_next(void){
+  semaphore_state_t state;
+  if state!=SemaphoreOff{
+    if state>1{
+	state++;
+      }
+    else if state=4{
+	state=2;
+      }
+    }
+}
+void semaphore_set(sempahore_state_t s){
+  state=s;
+}
