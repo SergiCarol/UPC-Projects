88ae6a0b22494926fe62b81f46cfdd959c2cb528
sergi.carol <sergi.carol@6045123d-4387-4ba5-964e-f94074b1feba>
2014-03-20 10:32:51 +0000

    Afegit el header del semaphore

---
+++ PBN/P4/semaphore.h
@@ -0,0 +1,15 @@
+#ifndef __SEMAPHORE_H
+#define __SEMAPHORE_H
+
+	typedef enum {
+		SemaphoreOff=0,
+		SemaphoreClear=2,
+		SempahoreApproach=3,
+		SempahoreStop=4,
+	}	sempahore_state_t
+
+	void sempahore_init(void)
+	void semamphore_next(void)
+	void sempaphore_set(sempahore_state_t s)
+
+#endif
