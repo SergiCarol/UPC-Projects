fcc2d6b44db6e0878af343625a66cd59e23c0319
sergi.carol <sergi.carol@6045123d-4387-4ba5-964e-f94074b1feba>
2014-03-27 09:39:06 +0000

    serial_device amb els includes correctes

--- PBN/P4/serial_device.c
+++ PBN/P4/serial_device.c
@@ -1,12 +1,13 @@
 #include <avr/sfr_defs.h>
 #include <avr/io.h>
 #include <stdint.h>
-
+#include <stdbool.h>
 void serial_init(void){
 	UBBR0H = 0x00;
 	UBRR0L = 0x67;
 	UCSR0A = 0x20;
 	UCSR0B = 0x98;
+	
 }
 
 uint8_t serial_get(void){
@@ -27,7 +28,7 @@ void serial_put(uint8_t c){
 
 bool serial_can_read(void){
 	if ((UCSR0A & (1<<7))== TRUE) return TRUE;
-	else return False;
+	else return FALSE;
 }
 
 
