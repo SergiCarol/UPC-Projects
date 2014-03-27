c2fd2c76c4f6d8114f376098ed4b8df988368a7e
enric.lenard <enric.lenard@6045123d-4387-4ba5-964e-f94074b1feba>
2014-03-27 10:54:29 +0000

    Creat la prova de semaphore

--- PBN/P4/semaphore.c
+++ PBN/P4/semaphore.c
@@ -1,20 +1,71 @@
 #include<stdio.h>
+#include<avr/scr_defs.h>
+#include<avr/io.h>
+
+DDRB=0xFF;
 
 void semaphore_init(void){
   semaphore_state_t state;
   state = SempahoreOff;
+  PD2=0;
+  PD3=0;
+  PD4=0;
 }
 void semaphore_next(void){
   semaphore_state_t state;
-  if state!=SemaphoreOff{
-    if state>1{
+  if state!=0{
+      if (state>1){
 	state++;
-      }
-    else if state=4{
-	state=2;
-      }
+	}
+      else if (state=4){
+	  state=2;
+	}
     }
+  swich(state){
+  case 2:
+    PD2=1;
+    PD3=0;
+    PD4=0;
+    break;
+  case 3:
+    PD2=0;
+    PD3=1;
+    PD4=0;
+    break;
+  case 4:
+    PD2=0;
+    PD3=0;
+    PD4=1;
+    break;
+  default:
+    PD2=0;
+    PD3=0;
+    PD4=0;
+  }
 }
+
 void semaphore_set(sempahore_state_t s){
   state=s;
+  swich(state){
+  case 2:
+    PD2=1;
+    PD3=0;
+    PD4=0;
+    break;
+  case 3:
+    PD2=0;
+    PD3=1;
+    PD4=0;
+    break;
+  case 4:
+    PD2=0;
+    PD3=0;
+    PD4=1;
+    break;
+  default:
+    PD2=0;
+    PD3=0;
+    PD4=0;
+  }
 }
+
