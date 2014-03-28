ac6b7def31007b784f96a80fc223ea1de4db38bd
sergi.carol <sergi.carol@6045123d-4387-4ba5-964e-f94074b1feba>
2014-03-28 19:30:57 +0000

    ACABAT GPIO, NO M'HO PUC CREURE

---
+++ PBN/P4/gpio_device.h
@@ -0,0 +1,18 @@
+#ifndef __GPIO_DEVICE_H
+#define __GPIO_DEVICE_H
+
+
+typedef enum {Input, Output} pin_direction_t;
+
+typedef struct {											// pin_t.port agafa el port
+	volatile uint8_t*  port /*= NULL*/;						
+	uint8_t pin;											// pin_t.pin agafa el pin del port
+} pin_t;
+//pin_t.port=NULL;
+
+pin_t pin_create(volatile uint8_t *port , uint8_t pin, pin_direction_t d);
+void pin_w(pin_t p,bool v);
+bool pin_r(pin_t p);
+void pin_toggle(pin_t p);
+
+#endif
