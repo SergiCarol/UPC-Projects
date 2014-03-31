3c29f8cfbf87d9f80580a71d2d2844287a28a582
sergi.carol <sergi.carol@6045123d-4387-4ba5-964e-f94074b1feba>
2014-03-31 08:47:37 +0000

    serial_device sense errors

--- PBN/P4/prova_semaphore.c
+++ PBN/P4/prova_semaphore.c
@@ -4,10 +4,9 @@
 int main(void){
   semaphore_init();
   semaphore_state_t s;
-  s=2;
+  s=3;
   semaphore_set(s);
   while(1){
- 	semaphore_next();
   _delay_loop_2(40000);
   _delay_loop_2(40000);
   _delay_loop_2(40000);
@@ -44,6 +43,7 @@ int main(void){
     _delay_loop_2(40000);
   _delay_loop_2(40000);
   _delay_loop_2(40000);
+  semaphore_next();
 
   }
 
