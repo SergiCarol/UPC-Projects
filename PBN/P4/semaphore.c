50ec3515604021f788b44afcbd0bcf5ef663fcd6
enric.lenard <enric.lenard@6045123d-4387-4ba5-964e-f94074b1feba>
2014-04-02 11:09:15 +0000

    Comentat main i semaphore

--- PBN/P4/semaphore.c
+++ PBN/P4/semaphore.c
@@ -3,17 +3,19 @@
 #include <avr/io.h>
 #include "gpio_device.h"
 #include <stdbool.h>
-
-static semaphore_state_t state;
+//Creem variables unicament per aquest modul.
+static semaphore_state_t state;  
 static pin_direction_t d;
 static pin_t verd, groc, vermell;
 
+//Inicialitzem el modul i posem l'estat a SemaphoreOff.
 void semaphore_init(void){
-  state = 0;
-  
+  state = 0;  
 }
+
+//Força le semafor a l'estat seguent.
 void semaphore_next(void){
-  
+  //Creem i associem els leds.
   d=Output;
   verd=pin_create(&PORTD, 5, d);
   groc=pin_create(&PORTD, 6, d);
@@ -22,20 +24,20 @@ void semaphore_next(void){
   if (state==0 || state==4)
     state=2;
   else
-    state++;
+    state++; //Incrementem valor de l'estat per passar al seguent.
  
   switch(state){
-  case 2:
+  case 2: //SemaphoreClear
     pin_w(verd,true);
     pin_w(groc, false);
     pin_w(vermell, false);
     break;
-  case 3:
+  case 3: //SemaphoreApproach
     pin_w(verd, false);    
     pin_w(groc,true);
     pin_w(vermell, false);
     break;
-  case 4:
+  case 4: //SemaphoreStop
     pin_w(verd, false);
     pin_w(groc, false);
     pin_w(vermell,true);
@@ -44,7 +46,7 @@ void semaphore_next(void){
   }
   
 }
-
+//Podem l'estat que ens indica 's'.
 void semaphore_set(semaphore_state_t s){
 
   d=Output;
@@ -54,22 +56,22 @@ void semaphore_set(semaphore_state_t s){
 
   state=s;
   switch(s){
-  case 2:
+  case 2: //SemaphoreClear
     pin_toggle(verd);
     pin_w(groc, false);
     pin_w(vermell, false);
     break;
-  case 3:
+  case 3: //SemaphoreApproach
     pin_w(verd, false);    
     pin_toggle(groc);
     pin_w(vermell, false);
     break;
-  case 4:
+  case 4: //SemaphoreStop
     pin_w(verd, false);
     pin_w(groc, false);
     pin_toggle(vermell);
     break;
-  default:
+  default: //SemaphoreOff
     pin_w(verd, false);
     pin_w(groc, false);
     pin_w(vermell, false);
