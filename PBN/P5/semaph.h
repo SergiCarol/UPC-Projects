1138e7f1c55aea4e94024c8af3e3c256d5a5c5ac
enric.lenard <enric.lenard@6045123d-4387-4ba5-964e-f94074b1feba>
2014-04-03 08:56:56 +0000

    semaph.

--- PBN/P5/semaph.h
+++ PBN/P5/semaph.h
@@ -1,1 +1,16 @@
-#include<
+#ifndef __SEMAPH_H
+#define __SEMAPH_H
+
+typedef enum{
+  SemOff,
+  SemClear,
+  SemApproach,
+  SemStop,
+} semaph_state_t;
+
+typedef struct{
+  lamp_t l;
+  semaph_state_t s;
+} semaph_t;
+
+#endif
