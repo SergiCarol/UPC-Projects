79c1c1e7c0aba9ad51d449afc1bac10ccdfd1061
sergi.carol <Sepharon@users.noreply.github.com>
2014-04-09 16:31:06 +0000

    lamp.c acabaaaat

---
+++ PBN/P5/lamp.c
@@ -0,0 +1,70 @@
+
+#include "lamp.h"
+#include <util/delay.h>
+
+// ACABAAAAAAAAAAAAT
+
+void lamp_init(lamp_t *const l , volatile uint8_t *port_green,uint8_t pin_green ,volatile uint8_t *port_yell,uint8_t pin_yell,volatile uint8_t *port_red,uint8_t pin_red){
+
+	(l)->green = pin_create(port_green,pin_green,Output);
+
+	(l)->yellow = pin_create(port_yell,pin_yell,Output);
+
+	(l)->red = pin_create(port_red,pin_red,Output);
+
+}
+
+void lamp_on(lamp_t l,color_t c){
+
+	if (c==Green) pin_w(l.green,true);
+	else if (c==Yellow) pin_w(l.yellow,true);
+	else if (c==Red) pin_w(l.red,true);
+
+}
+
+void lamp_off(lamp_t l, color_t c){
+
+	if (c==Green) pin_w(l.green,false);
+	else if (c==Yellow) pin_w(l.yellow,false);
+	else if (c==Red) pin_w(l.red,false);
+}
+void lamp_toggle(lamp_t l ,color_t c){
+	if (c==Green) pin_toggle(l.green);
+	else if (c==Yellow) pin_toggle(l.yellow);
+	else if (c==Red) pin_toggle(l.red);
+}
+bool lamp_is_on(lamp_t l , color_t c){
+
+	bool a;
+	
+	if (c==Green) a = pin_r(l.green);
+	else if (c==Yellow) a = pin_r(l.yellow);
+	else if (c==Red) a = pin_r(l.red);
+
+	return a;
+
+}
+void main(void){
+
+	lamp_t t;
+	color_t c;
+
+	lamp_init(&t ,&PORTD, 5 ,&PORTD, 6,&PORTD,7);
+	while (1){
+		c=Green;
+		_delay_ms(1000);
+		lamp_toggle(t,c);
+		c=Yellow;
+		_delay_ms(1000);
+		lamp_toggle(t,c);
+		c=Red;
+		_delay_ms(1000);
+		lamp_toggle(t,c);
+
+	}
+
+
+
+
+
+}
