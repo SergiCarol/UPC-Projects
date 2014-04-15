8787b3dae2f3ad001aa224eacebd019f9bd3203d
sergi.carol <sergi.carol@6045123d-4387-4ba5-964e-f94074b1feba>
2014-04-15 19:05:42 +0000

    serial funciona més o menys

--- PBN/P5/queue.c
+++ PBN/P5/queue.c
@@ -1,34 +1,50 @@
 #include <stdio.h>
-
+#include <stdbool.h>
+#include <avr/io.h>
+#include "queue.h"
 /*Esto es un comentario mal tabulado en Sublime Text*/ 
                                     /*Esto es un comentario mal tabulado en emacs*/
 
 
-
 void queue_empty(queue_t *const q){
-  /*Inicialitza la cua q a l'estat buit.*/
+  /*Inicialitza la cua q a l'estat buit.*/  
+	q->punter = 0;
 }
 
 bool queue_is_empty(const queue_t *const q){
   /*Retorna true si q és buida.*/
+	if(q->punter == 0) return 1;
+	else return 0;
 }
 
-bool queue_is_full(const queue *const q){
+bool queue_is_full(const queue_t *const q){
   /*Retorna true si q és plena.*/
+	if(q->punter == ELEMENTS-1) return 1;
+	else return 0;
 }
 
 void queue_enqueue(queue_t *const q, uint8_t v){
   /*Encua l'element v a la cua q. Si la cua és plena no encua res.*/
+	if(q->punter != ELEMENTS-1){
+		q->memoria[q->punter]=v;
+		q->punter++;
+	}
 }
 
 void queue_dequeue(queue_t *const q){
   /*Desencua l¡element del davant de la cua q. Si la cua és buida no
 fa res.*/
+	if(q->punter != ELEMENTS-1){
+		q->memoria[q->punter]=0;
+		q->punter--;
+	}
 }
 
 uint8_t queue_front(const queue_t *const q){
-  /*Retorna l'element del davant de la cua q. Si la cua és buida retorna
+/*Retorna l'element del davant de la cua q. Si la cua és buida retorna
 un valor arbitrari.*/
+	if (queue_is_empty(q)) return 0;
+    else return q->memoria[q->punter];
 }
 
 
