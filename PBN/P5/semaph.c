7c31884094536d8a9e4a577baf092c7d5562d1ea
enric.lenard <enric.lenard@6045123d-4387-4ba5-964e-f94074b1feba>
2014-04-15 15:25:14 +0000

    maestro

--- PBN/P5/semaph.c
+++ PBN/P5/semaph.c
@@ -1,55 +1,51 @@
 #include <stdio.h>
-#include "lamp.h"
-#include "semaph.h"
 #include <avr/io.h>
 #include <stdbool.h>
+#include "lamp.h"
+#include "semaph.h"
+
 
 //Creem variables unicament per aquest modul.
-static semaph_state_t state;  
-static lamp_t l;
+static semaph_t state;  
 /*static lamp_t green, yellow, red;*/
 
 //Inicialitzem el modul i posem l'estat a SemaphoreOff.
-void semaph_init(semaph_t *const s, volatile uint8_t *port_green,
-uint8_t pin_green, volatile uint8_t *port_yell, uint8_t pin_yell,volatile uint8_t *port_red, uint8_t pin_red){
-  state = SemOff;  
+void semaph_init(semaph_t *const s, volatile uint8_t *port_green, uint8_t pin_green, volatile uint8_t *port_yell, uint8_t pin_yell,volatile uint8_t *port_red, uint8_t pin_red){
+  lamp_init(&((s)->l) , port_green, pin_green , port_yell, pin_yell, port_red, pin_red);
+  (*s).s = SemOff;  
 }
 
-void semaph_set(semaph_t *const sem, semaph_state_t s){//Fer amb LAMP
-  color_t verd, groc, vermell;
-  verd=Green;
-  groc=Yellow;
-  vermell=Red;
+void semaph_set(semaph_t *const sem, semaph_state_t s){
   //Fiquem el estat global del semafor al estat s.
-  state=s; 
-  switch(state){
+  state.s=s; 
+  switch(state.s){
     //SemaphoreClear
   case SemClear: 
     //Comuta el valor del pin.
-    lamp_on(sem.l, verd);
-    lamp_off(sem.l, groc);
-    lamp_off(sem.l, vermell);
+    lamp_on((*sem).l, Green);
+    lamp_off((*sem).l, Yellow);
+    lamp_off((*sem).l, Red);
     break;
     //SemaphoreApproach
   case SemApproach: 
-    lamp_off(sem.l, verd);    
-    lamp_toggle(sem.l, groc);
-    lamp_off(sem.l, vermell);
+    lamp_off((*sem).l, Green);
+    lamp_on((*sem).l, Yellow);
+    lamp_off((*sem).l, Red);
     break;
     //SemaphoreStop
   case SemStop: 
-    lamp_off(sem.l, verd);
-    lamp_off(sem.l, groc);
-    lamp_toggle(sem.l, vermell);
+    lamp_off((*sem).l, Green);
+    lamp_off((*sem).l, Yellow);
+    lamp_on((*sem).l, Red);
     break;
     //SemaphoreOff
   default: 
-    lamp_off(sem.l, verd);
-    lamp_off(sem.l, groc);
-    lamp_off(sem.l, vermell);
+    lamp_off((*sem).l, Green);
+    lamp_off((*sem).l, Yellow);
+    lamp_off((*sem).l, Red);
   }
 }
 
 semaph_state_t semaph_get(const semaph_t sem){
-  return sem.state;
+  return sem.s;
 }
