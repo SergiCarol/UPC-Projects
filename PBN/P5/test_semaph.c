5c503802372f45f3da85c79f9a457b5f19b96b81
enric.lenard <enric.lenard@6045123d-4387-4ba5-964e-f94074b1feba>
2014-04-15 15:42:55 +0000

    prova semaph

--- PBN/P5/test_semaph.c
+++ PBN/P5/test_semaph.c
@@ -4,14 +4,16 @@
 int main(void){
   
   semaph_t s;
-
+  semaph_state_t a;
+  a=SemClear;
   (*s).s=SemOff;
   semaph_init(&s, &PORTD, 5, &PORTD, 6, &PORTD, 7);
-  semaph_set(s);
+  semaph_set(s, a);
 
   while(1){
     _delay_loop_ms(100);
-    semaph_get();
+    a=semaph_get(s);
+    semaph_set(s,a);
   }
   return 0;
 }
