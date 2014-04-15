9c799dc910525af2b301f88ede16d0962cc37da2
enric.lenard <enric.lenard@6045123d-4387-4ba5-964e-f94074b1feba>
2014-04-15 10:39:25 +0000

    Acavat modul semaph

--- PBN/P5/semaph.c
+++ PBN/P5/semaph.c
@@ -6,49 +6,50 @@
 
 //Creem variables unicament per aquest modul.
 static semaph_state_t state;  
-//static pin_direction_t d;
-static lamp_t verd, groc, vermell;
+static lamp_t l;
+/*static lamp_t green, yellow, red;*/
 
 //Inicialitzem el modul i posem l'estat a SemaphoreOff.
-void semaph_init(semaph_t *const s, volatile unit8_t *port, unit8_t pin){
+void semaph_init(semaph_t *const s, volatile uint8_t *port_green,
+uint8_t pin_green, volatile uint8_t *port_yell, uint8_t pin_yell,volatile uint8_t *port_red, uint8_t pin_red){
   state = SemOff;  
 }
 
 void semaph_set(semaph_t *const sem, semaph_state_t s){//Fer amb LAMP
-  d=Output;
-  verd=pin_create(&PORTD, 5, d);
-  groc=pin_create(&PORTD, 6, d);
-  vermell=pin_create(&PORTD, 7, d);
+  color_t verd, groc, vermell;
+  verd=Green;
+  groc=Yellow;
+  vermell=Red;
   //Fiquem el estat global del semafor al estat s.
   state=s; 
-  switch(s){
+  switch(state){
     //SemaphoreClear
-  case 2: 
+  case SemClear: 
     //Comuta el valor del pin.
-    pin_w(verd,true);  
-    pin_w(groc, false);
-    pin_w(vermell, false);
+    lamp_on(sem.l, verd);
+    lamp_off(sem.l, groc);
+    lamp_off(sem.l, vermell);
     break;
     //SemaphoreApproach
-  case 3: 
-    pin_w(verd, false);    
-    pin_toggle(groc);
-    pin_w(vermell, false);
+  case SemApproach: 
+    lamp_off(sem.l, verd);    
+    lamp_toggle(sem.l, groc);
+    lamp_off(sem.l, vermell);
     break;
     //SemaphoreStop
-  case 4: 
-    pin_w(verd, false);
-    pin_w(groc, false);
-    pin_toggle(vermell);
+  case SemStop: 
+    lamp_off(sem.l, verd);
+    lamp_off(sem.l, groc);
+    lamp_toggle(sem.l, vermell);
     break;
     //SemaphoreOff
   default: 
-    pin_w(verd, false);
-    pin_w(groc, false);
-    pin_w(vermell, false);
+    lamp_off(sem.l, verd);
+    lamp_off(sem.l, groc);
+    lamp_off(sem.l, vermell);
   }
 }
 
 semaph_state_t semaph_get(const semaph_t sem){
-  return sem.s;
+  return sem.state;
 }
