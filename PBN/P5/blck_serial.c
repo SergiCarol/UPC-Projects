cf4e023916df628709342db7ad06e7f6a5a17a93
sergi.carol <Sepharon@users.noreply.github.com>
2014-04-16 13:36:28 +0000

    arreglant merdes varies

--- PBN/P5/blck_serial.c
+++ PBN/P5/blck_serial.c
@@ -1,22 +1,40 @@
 #include "blck_serial.h"
-
-
+#include <util/delay.h>
 void print(char s[]){
-	uint8_t i, c;
-
-	for (i=0;c!='\0';i++){
-		c=s[i]
-		serial_put(c);
+	uint8_t i=0, c;
+    while(s[i]!='\0'){
+      	serial_put((uint8_t)s[i]);
+      	i++;
 	}
-	serial_put('\n');
+
 }
 
 int readline(char s[],uint8_t m){
-	uint8_t i=0,a;
-	while (m!=i || ispunct()){
+	int i=0,a;
+	while (m!=i || ispunct(s[i])){
 		a=serial_get();
 		s[i] = a;
 		i++;
 	}
 	return i;
 }
+
+
+int main(void){
+  	uint8_t i=0,c;
+  	serial_open();
+  	char j[]="aasdfghasdfgbh";
+  	while(j[i]!='\0'){
+    	serial_put(j[i]);
+    	i+=1;
+	}
+ 	//print(s);
+ 	/*serial_put(s[0]);
+	serial_put(s[1]);
+	serial_put(s[2]);
+	serial_put(s[3]);
+	serial_put(s[4]);
+	serial_put(s[5]);
+	serial_put('\0');*/
+	serial_close();
+} 
