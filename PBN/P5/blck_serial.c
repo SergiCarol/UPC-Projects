d0ef023d00567a9c081ce54173d75db54c701098
sergi.carol <Sepharon@users.noreply.github.com>
2014-04-16 15:01:50 +0000

    blck funcionant YEAH BITCHES

--- PBN/P5/blck_serial.c
+++ PBN/P5/blck_serial.c
@@ -3,27 +3,19 @@
 void print(char s[]){
 	uint8_t i=0, c;
     while(s[i]!='\0'){
-      	serial_put((uint8_t)s[i]);
+      	serial_put(s[i]);
       	i++;
 	}
 
 }
 
 int readline(char s[],uint8_t m){
-	int i=0,a;
+	uint8_t i=0,a;
 	while (m!=i || ispunct(s[i])){
 		a=serial_get();
+		serial_put(a);
 		s[i] = a;
 		i++;
 	}
 	return i;
 }
-
-
-int main(void){
-  	uint8_t i=0,c;
-  	serial_open();
-  	char j[]="aasdfghasdfgbh";
-  	print(j);
-	serial_close();
-} 
