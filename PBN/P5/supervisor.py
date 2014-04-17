7b37a180216c9b6a94eeca26a5ce7414cbbf9f48
sergi.carol <sergi.carol@6045123d-4387-4ba5-964e-f94074b1feba>
2014-04-17 17:43:21 +0000

    afegit test

--- PBN/P5/supervisor.py
+++ PBN/P5/supervisor.py
@@ -3,34 +3,58 @@
 from interpret import Interpret
 import serial
 
-def start():
-    #Escribim pel port serie.
-    ser.write('R')
-    # Llegim el contingut del port serie.
+def forceA():
+    ser.write("forceA")
     a=ser.readline()
-    # En el cas de que sigui un restart és que tot ha sortit bé.
-    if a == "RESTART\n":
-        print "RESTART"
-    # Si no, es que ja està funcionant.
+    if a == "encesA\n":
+        print "El semafor A està en verd"
     else:
-        print "Error :  El semafor ja està inicilitzat"
+        print "Error: el semafor no s'ha pogut ficar en verd"
+
+def forceB():
+    ser.write("forceB")
+    a=ser.readline()
+    if a == "encesB\n":
+        print "El semafor B està en verd"
+    else:
+        print "Error: el semafor no s'ha pogut ficar en verd"
+
 def stop():
-    ser.write('S')
+    ser.write("control_off")
     b=ser.readline()
-    if b == "SHUTDOWN\n":                                                   
-        print "SHUTDOWN"
+    if b == "apagat\n":                                                   
+        print "Shutdown"
     else:
         print "Error : La comanda stop només es pot utilitzar si el semafor està ences"
 
 def ajuda():
-    print " Les comanandes són : start , stop , emergency"
-def emergency():
-    ser.write('E')
+    print " Les comanandes són : start , stop , estat_A, estat_B, force_A i force_B"
+
+def start():
+    ser.write("control_on")
     a=ser.readline()
-    if a== "EMERGENCY\n":
-        print "EMERGENCY"
+    if a== "ences\n":
+        print "Els semafors s'han engegat correctament"
     else:
-        print "Error : La comanda emergency no es pot utilitzar com inicialitzador ni com a restart després d'un stop "
+        print "Error 404: Semafors not found "
+
+def stateA():
+	ser.write("estatA")
+    a=ser.readline()
+   	if a!="semoff\n":
+   		print a
+   	else:
+   		print "Error"
+
+def stateB():
+	ser.write("estatB")
+    a=ser.readline()
+   	if a!="semoff\n":
+   		print a
+   	else:
+   		print "Error"
+
+
 if __name__=="__main__":
     # Incialitzem el port serie.
     ser=serial.Serial('/dev/ttyACM0',9600)
@@ -40,7 +64,10 @@ if __name__=="__main__":
     I.afegeix_ordre("ajuda",ajuda) 
     I.afegeix_ordre("start",start)
     I.afegeix_ordre("stop",stop)
-    I.afegeix_ordre("emergency",emergency)
+    I.afegeix_ordre("estat_A",stateA)
+    I.afegeix_ordre("estat_B",stateB)
+    I.afegeix_ordre("force_A",forceA)
+    I.afegeix_ordre("force_B",forceA)
     I._prompt="***"
     I.run()
     ser.close()
