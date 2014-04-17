d3079461f9de9d3d6a94d28dd89f1285d96e84de
sergi.carol <sergi.carol@6045123d-4387-4ba5-964e-f94074b1feba>
2014-04-17 15:34:00 +0000

    queue acabat, interpret afegit

--- PBN/P5/prova_blckserial.c
+++ PBN/P5/prova_blckserial.c
@@ -1,15 +1,37 @@
 #include "blck_serial.h"
+#include <stdbool.h>
+#include <avr/io.h>
 
-int main(void){
+
+
+bool test(void){
+
   uint8_t i=0,c;
-  serial_open();
   char s[]="acabaaat\n\r";
   char j[]="";
+  char y[]="";
   c=readline(j,9);
   print (s);
   serial_put(c+0x30);
+  return true;
+}
+
+
+void test2(void){
+  while(1){
+    serial_put(serial_get());
+  }
+}
+
+int main(void){
+
+  bool a;
+
+  serial_open();
+  test2();
   serial_put('\n');
   serial_put('\r');
-	serial_close();
-	return 0;
+  serial_close();
+  PORTD=0xFF ;
+  return 0;
 } 
