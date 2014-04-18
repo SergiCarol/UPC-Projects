2d56e95ef37b096e07e029a1dd70806c9a292302
sergi.carol <sergi.carol@6045123d-4387-4ba5-964e-f94074b1feba>
2014-04-18 16:00:52 +0000

    lo de abans era broma

--- PBN/P5/queue.c
+++ PBN/P5/queue.c
@@ -1,60 +1,63 @@
+#include <stdio.h>
 #include <stdbool.h>
-#include <inttypes.h>
-#include <avr/io.h>
 #include <util/atomic.h>
 #include "queue.h"
+/*Esto es un comentario mal tabulado en Sublime Text*/ 
+                                    /*Esto es un comentario mal tabulado en emacs*/
+
 
 void queue_empty(queue_t *const q){
-    q->readPointer= 0;   
-    q->writePointer=0;
-    q->cont=0;
+  /*Inicialitza la cua q a l'estat buit.*/  
+	q->comptador = 0;
+	q->punter_lectura = 0;
+	q->punter_escritura = 0;
 }
 
 bool queue_is_empty(const queue_t *const q){
-  //return (q->writePointer==q->readPointer);   //TODO: He de comprobar si esta full???
-  return ((q->cont)==0);
+  /*Retorna true si q és buida.*/
+	if(q->comptador == 0) return true;
+	else return false;
 }
 
 bool queue_is_full(const queue_t *const q){
-  if (q->cont==MIDA_BUFF-1){}
-  return (q->cont==MIDA_BUFF-1);
+  /*Retorna true si q és plena.*/
+	if(q->comptador == ELEMENTS-1) return true;
+	else return false;
 }
 
 void queue_enqueue(queue_t *const q, uint8_t v){
-  ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
-  {
-    if (!queue_is_full(q)) {
-      
-      q->queue[q->writePointer]=v;
-      /*Si al incrementar el punter sobrepasem la capacitat torna un zero*/
-      //++(q->writePointer)==MIDA_BUFF? 0: ++(q->writePointer);
-      int a=(q->writePointer)+1;
-      a==MIDA_BUFF ? (q->writePointer)=0: ((q->writePointer)+=1);
-      (q->cont)+=1;
-    }
-  }
+  /*Encua l'element v a la cua q. Si la cua és plena no encua res.*/
+	ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
+		if(q->comptador != ELEMENTS-1){
+			q->memoria[q->punter_escritura]=v;
+			q->comptador++;
+			q->punter_escritura++;
+			if(q->punter_escritura == ELEMENTS) q->punter_escritura = 0;
+		}
+	}
 }
 
 void queue_dequeue(queue_t *const q){
-  ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
-  {
-    if (!queue_is_empty(q)) {
-      /*Si al incrementar el punter sobrepasem la capacitat torna un zero*/
-      //++(q->readPointer)==MIDA_BUFF? 0: ++(q->readPointer);
-      int b=(q->readPointer)+1;
-      b==MIDA_BUFF ? (q->readPointer)=0: ((q->readPointer)+=1);
-      (q->cont)-=1;
-    }
-  }
+  /*Desencua l¡element del davant de la cua q. Si la cua és buida no
+fa res.*/
+	ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
+		if(q->comptador != ELEMENTS-1){
+			q->punter_lectura++;
+			q->comptador--;
+			if(q->punter_lectura == ELEMENTS) q->punter_lectura = 0;
+			
+		}
+	}
 }
 
 uint8_t queue_front(const queue_t *const q){
-  ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
-  {
-    if (queue_is_empty(q)) 
-      return 0;
-    else
-      return q->queue[q->readPointer];
-  }
+/*Retorna l'element del davant de la cua q. Si la cua és buida retorna
+un valor arbitrari.*/
+	if (queue_is_empty(q)) return 0;
+    else{
+    	return q->memoria[q->punter_lectura];
+    }
 }
 
+
+//http://www.sites.upiicsa.ipn.mx/polilibros/portal/Polilibros/P_terminados/EstrRepreDat/Files/listas_circulares.html
--- PBN/P5/queue.h
+++ PBN/P5/queue.h
@@ -1,16 +1,16 @@
-#ifndef QUEUE_H
-#define QUEUE_H
+#ifndef __QUEUE_H
+#define __QUEUE_H
 
-#define MIDA_BUFF 20
 #include <stdbool.h>
 #include <inttypes.h>
+#define ELEMENTS 55
 
 typedef struct
 {
-  uint8_t queue[MIDA_BUFF];
-  uint8_t writePointer;
-  uint8_t readPointer;
-  uint8_t cont;       
+  uint8_t memoria[ELEMENTS];
+  uint8_t comptador;
+  uint8_t punter_lectura;
+  uint8_t punter_escritura;     
 } queue_t;
 
 void queue_empty(queue_t *const q);
