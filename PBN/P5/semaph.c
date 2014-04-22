a6f081ac0eaf3b83f9bc65299c1a9c3ee52414fa
enric.lenard <enric.lenard@6045123d-4387-4ba5-964e-f94074b1feba>
2014-04-22 15:35:34 +0000

    Comentat el modul semaph.c

--- PBN/P5/semaph.c
+++ PBN/P5/semaph.c
@@ -4,21 +4,19 @@
 #include "lamp.h"
 #include "semaph.h"
 
-
-
 //Inicialitzem el modul i posem l'estat a SemaphoreOff.
 void semaph_init(semaph_t *const s, volatile uint8_t *port_green, uint8_t pin_green, volatile uint8_t *port_yell, uint8_t pin_yell,volatile uint8_t *port_red, uint8_t pin_red){
+  //Els parametres port i pin indiquen on es connecta la lampada.
   lamp_init(&((s)->l) , port_green, pin_green , port_yell, pin_yell, port_red, pin_red);
   (*s).s = SemOff;  
 }
-
+//Posem l'estat s al semafor sem.
 void semaph_set(semaph_t *const sem, semaph_state_t s){
-  //Fiquem el estat global del semafor al estat s.
   (*sem).s=s; 
+  //Indiquem els valors de cada estat.
   switch(s){
     //SemaphoreClear
   case SemClear: 
-    //Comuta el valor del pin.
     lamp_on((*sem).l, Green);
     lamp_off((*sem).l, Yellow);
     lamp_off((*sem).l, Red);
@@ -42,7 +40,7 @@ void semaph_set(semaph_t *const sem, semaph_state_t s){
     lamp_off((*sem).l, Red);
   }
 }
-
+//Retorna l'estat del semafor sem.
 semaph_state_t semaph_get(const semaph_t sem){
   return sem.s;
 }
