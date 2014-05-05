b4a3f7bab9217706a81718e34d084ed700817de4
sergi.carol <Sepharon@users.noreply.github.com>
2014-05-05 08:33:45 +0000

    Timer acabat

--- PBN/P6/timer.c
+++ PBN/P6/timer.c
@@ -9,6 +9,7 @@
 #define Target ((Input_Frequency/(Prescale*Target_time) - 1))
 
 // Aixó esta ficat a la practica
+// Entenc que la entry és per guardar els valors de temps de la funció, per aixó tenim més espais per casa funcio
 typedef struct {
 	uint8_t remaining, every, ntimes;
 	timer_callback_t callback;
@@ -31,10 +32,12 @@ void timer_init(void){
 }
 
 timer_handler_t timer_after(uint8_t ticks, timer_callback_t f){
+	// Funcio més inutil que he vist en me vida
 	return timer_ntimes(1,ticks,f);
 }
 
 timer_handler_t timer_every(uint8_t ticks, timer_callback_t f){
+	// Funcio més inutil que he vist en me vida 2.0
 	return timer_ntimes(0,ticks,f);
 }
 
@@ -42,10 +45,13 @@ timer_handler_t timer_ntimes(uint8_t n, uint8_t ticks, timer_callback_t f){
 
 
 	uint8_t i;
+	// Recorrem tota la "taula" per buscar algun lloc en que no hi haguem ficat res
 	for (i=0;i!=N;i++){
 		if (i>N) return TIMER_ERR;
-		if (tt.every==0){
+		// Aixó nose si esta bé, jo entenc que el every es per dir si esta ocupat o no
+		if (tt.t[i].every==0){
 			tt.t[i].remaning = ticks;
+			// PREGUNTAR AL SBS
 			tt.t[i].every = ticks;
 			tt.t[i].ntimes = n;
 			tt.t[i].entry.callback = f; 
@@ -58,9 +64,39 @@ timer_handler_t timer_ntimes(uint8_t n, uint8_t ticks, timer_callback_t f){
 			return i;
 		}
 	}
+	// Si no troba cap espai but que retorni el error aquet
 	return TIMER_ERR;
 }
 
+void timer_cancel (timer_handler_t h){
+
+	tt.t[h].every = 0;
+}
+
+void timer_cancel_all (void){
+
+	uint8_t i;	
+	for (i=0;i!=N;i++){
+		tt.t[i].every=0;
+	}
+}
+
+
 ISR(TIMER1_COMPA_vect){
 
+	uint8_t i;
+	for (i=0;i!=N,i++){
+		if (tt.t[i].every != 0){
+			tt.t[i].remaning--
+		}
+		if ((tt.t[i].remaning == 0) && (tt.t[i].ntimes = 1)){
+			tt.every = 0;
+			tt.t[i].callback;
+		else if ((tt.t[i].remaning == 0) && (tt.t[i].ntimes = 0)){ 
+			tt.t[i].remaning = tt.t[i].every ;
+			tt.t[i].callback;
+		}
+		else tt.[i].ntimes--;
+
+	}
 }	
