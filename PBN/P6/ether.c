35b57939da9ef28049827d7b0715921d1697e8a0
sergi.carol <Sepharon@users.noreply.github.com>
2014-05-12 10:37:03 +0000

    afegit tar

--- PBN/P6/ether.c
+++ PBN/P6/ether.c
@@ -19,71 +19,71 @@ static void automat(void);
 
 
 void ether_init(void){
-	modulator_init();
-	timer_init();
-	queue_empty(&m);
+  modulator_init();
+  timer_init();
+  queue_empty(&m);
 }
 
 void ether_put(uint8_t c){
-	bool a;
-	a= queue_is_empty(&m);
-	// Ens esperem mentres linia cua estigui plena
-	while (queue_is_full(&m));
-	// Un cop ho deixa de estar afegim el caracter C a la cua
-	queue_enqueue(&m,c);
-	/* Basicament si no hem acabat de transmetre ens esperarem, ja que la mc_i encara 
-	   estaria sobre un caracter que podem transmetre (. o -) */
-	if (a && !mchar_valid(mchar)) automat();
+  bool a;
+  a= queue_is_empty(&m);
+  // Ens esperem mentres linia cua estigui plena
+  while (queue_is_full(&m));
+  // Un cop ho deixa de estar afegim el caracter C a la cua
+  queue_enqueue(&m,c);
+  /* Basicament si no hem acabat de transmetre ens esperarem, ja que la mc_i encara 
+     estaria sobre un caracter que podem transmetre (. o -) */
+  if (a && !mchar_valid(mchar)) automat();
 }
 
 static void automat (void){
-	uint8_t a;
-	if (queue_is_empty(&m)==true){
+  uint8_t a;
+  if (queue_is_empty(&m)==true){
 		// Aqui hi va algu??? no,no????
-	}
-	// Crec que aixo es podria fer més façil, pero quedaria una linia tope llarga 
-	else{
-		a=queue_front(&m);
-		t=mtbl_a2m(a);
-		mchar=mchar_iter(t);
-		queue_dequeue(&m);
-		// Si el caracter es un punt, pues fem un punt
-		// Recordar que el mchar_next dona el valor del que estem ara i l'avança
-		if (mchar_next(&mchar)==MDot) dot();
-		// Si no, fem una ratlla
-		else dash();
-		}
+  }
+  // Crec que aixo es podria fer més façil, pero quedaria una linia tope llarga 
+  else{
+    a=queue_front(&m);
+    t=mtbl_a2m(a);
+    mchar=mchar_iter(t);
+    queue_dequeue(&m);
+    // Si el caracter es un punt, pues fem un punt
+    // Recordar que el mchar_next dona el valor del que estem ara i l'avança
+    if (mchar_next(&mchar)==MDot) dot();
+    // Si no, fem una ratlla
+    else dash();
+  }
 }
 
 
 static void dot(void){
-	// Si es una punt engegem el modulator i ens esperem el temps de punt
-	modulator_set(true);
-	timer_after(TIMER_MS(DOT),down);
+  // Si es una punt engegem el modulator i ens esperem el temps de punt
+  modulator_set(true);
+  timer_after(TIMER_MS(DOT),down);
 }
 
 static void dash(void){
-	// Lo mateix pero amb temps de ratlla
-	modulator_set(true);
-	timer_after(TIMER_MS(DASH),down);
+  // Lo mateix pero amb temps de ratlla
+  modulator_set(true);
+  timer_after(TIMER_MS(DASH),down);
 }
 
 static void gap(void){
-	// En el cas de que sigui un punt pues anem a fer un DOT
-	if (mchar_next(&mchar)==MDot) dot();
-	// Si no, una ratlla
-	else dash();
+  // En el cas de que sigui un punt pues anem a fer un DOT
+  if (mchar_next(&mchar)==MDot) dot();
+  // Si no, una ratlla
+  else dash();
 }
 
 static void down(void){
-	modulator_set(false);
-	// Comprobem si el caracter que volem enviar es valid
-	if (mchar_valid(mchar)){
-		// Si ho es fem un silenci de GAP
-		timer_after(TIMER_MS(GAP),gap);
-	}
-	// En cas contrari fem pausa de lletra
-	else timer_after(TIMER_MS(LETTERGAP),automat);
+  modulator_set(false);
+  // Comprobem si el caracter que volem enviar es valid
+  if (mchar_valid(mchar)){
+    // Si ho es fem un silenci de GAP
+    timer_after(TIMER_MS(GAP),gap);
+  }
+  // En cas contrari fem pausa de lletra
+  else timer_after(TIMER_MS(LETTERGAP),automat);
 }
 
 
--- PBN/P6/timer.c
+++ PBN/P6/timer.c
@@ -11,100 +11,98 @@
 // Aixó esta ficat a la practica
 // Entenc que la entry és per guardar els valors de temps de la funció, per aixó tenim més espais per casa funcio
 typedef struct {
-	uint8_t remaining, every, ntimes;
-	timer_callback_t callback;
+  uint8_t remaining, every, ntimes;
+  timer_callback_t callback;
 } entry;
 
 static struct {
-	entry t[N];
-	//Numero de entrades valides
+  entry t[N];
+  //Numero de entrades valides
 	uint8_t n;
 }tt;
 
 
 void timer_init(void){
-
-	//Configurem el timer
-	TCCR1A = 0;
-  	TCCR1B = (_BV(WGM12) | _BV(CS11) | _BV(CS10));  
-  	OCR1AH = (uint8_t)(Target >> 8);    
-  	OCR1AL = (uint8_t)(Target);
-
-  	timer_cancel_all();
+  
+  //Configurem el timer per que entri a la interrupcio cada 10 ms
+  TCCR1A = 0;
+  TCCR1B = (_BV(WGM12) | _BV(CS11) | _BV(CS10));  
+  OCR1AH = (uint8_t)(Target >> 8);    
+  OCR1AL = (uint8_t)(Target);
+  
+  timer_cancel_all();
 }
 
 timer_handler_t timer_after(uint8_t ticks, timer_callback_t f){
-	// Funcio més inutil que he vist en me vida
-	return timer_ntimes(1,ticks,f);
+  return timer_ntimes(1,ticks,f);
 }
 
 timer_handler_t timer_every(uint8_t ticks, timer_callback_t f){
-	// Funcio més inutil que he vist en me vida 2.0
-	return timer_ntimes(0,ticks,f);
+  return timer_ntimes(0,ticks,f);
 }
 
 timer_handler_t timer_ntimes(uint8_t n, uint8_t ticks, timer_callback_t f){
-
-
-	uint8_t i;
-	// Recorrem tota la "taula" per buscar algun lloc en que no hi haguem ficat res
-	for (i=0;i!=N;i++){
-		if (i>N) return TIMER_ERR;
-		// Aixó nose si esta bé, jo entenc que el every es per dir si esta ocupat o no
-		if (tt.t[i].every==0){
-			tt.n++;
-			tt.t[i].remaining = ticks;
-			// PREGUNTAR AL SBS
-			tt.t[i].every = ticks;
-			tt.t[i].ntimes = n;
-			tt.t[i].callback = f; 
-		
-		// Si no hem planificat abans activem el timer 
-			if (tt.n > 0){
-				TCNT1 = 0;
-				TIMSK1 |= _BV(OCIE1A);
-			}
-			return i;
-		}
-	}
+  
+  
+  uint8_t i;
+  // Recorrem tota la "taula" per buscar algun lloc en que no hi haguem ficat res
+  for (i=0;i!=N;i++){
+    if (i>N) return TIMER_ERR;
+    // Si la posicio no esta ocupada hi escribim
+    if (tt.t[i].every==0){
+      tt.n++;
+      tt.t[i].remaining = ticks;
+      tt.t[i].every = ticks;
+      tt.t[i].ntimes = n;
+      tt.t[i].callback = f; 
+      
+      // Si no hem planificat abans activem el timer 
+      if (tt.n > 0){
+	TCNT1 = 0;
+	TIMSK1 |= _BV(OCIE1A);
+      }
+      return i;
+    }
+  }
 	// Si no troba cap espai but que retorni el error aquet
-	return TIMER_ERR;
+  return TIMER_ERR;
 }
 
 void timer_cancel (timer_handler_t h){
-	if (h<N) tt.t[h].every = 0;
+  if (h<N) tt.t[h].every = 0;
 }
 
 void timer_cancel_all (void){
-
-	uint8_t i;	
-	for (i=0;i!=N;i++){
-		tt.t[i].every=0;
-	}
+  
+  uint8_t i;	
+  for (i=0;i!=N;i++){
+    tt.t[i].every=0;
+  }
 }
 
 
 ISR(TIMER1_COMPA_vect){
-	uint8_t i;
-	for (i=0;i!=N;i++){
-		
-		if (tt.t[i].every != 0){
-			
-			tt.t[i].remaining--;
-			
-			if (tt.t[i].remaining == 0){
-				
-				 (tt.t[i].callback)();
-
-				if (tt.t[i].ntimes == 1) tt.t[i].every = 0;
-				
-				else if (tt.t[i].ntimes == 0) tt.t[i].remaining = tt.t[i].every ;
-				
-				else{
-					 tt.t[i].ntimes--;
-					 tt.t[i].remaining = tt.t[i].every;
-				}
-			}
-		}	
+  uint8_t i;
+  // Recorrem la taula
+  for (i=0;i!=N;i++){
+    // Entrem dintre els valors que hem escrit
+    if (tt.t[i].every != 0){
+      // Hi restem un tick
+      tt.t[i].remaining--;
+      // Si hem acabat de restar...
+      if (tt.t[i].remaining == 0){
+	// Cridem al funcio
+	(tt.t[i].callback)();
+	// En el cas de que aquet fos l'ultim cop que la teniem que cridar fiquem el every a 0
+	if (tt.t[i].ntimes == 1) tt.t[i].every = 0;
+	// Si es te que fer indefinidament tornem a guardar el valor de ticks al remaining
+	else if (tt.t[i].ntimes == 0) tt.t[i].remaining = tt.t[i].every ;
+	// En cas de que no fos cap dels de abans restem un ntimes i tornem a ficar el valor de remaning a every
+	else{
+	  tt.t[i].ntimes--;
+	  tt.t[i].remaining = tt.t[i].every;
 	}
+      }
+    }	
+  }
 }	
