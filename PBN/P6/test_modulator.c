ab6f6bdba247f7cfcd4b41598afd2a4db333dc88
sergi.carol <Sepharon@users.noreply.github.com>
2014-05-21 08:37:40 +0000

    afegits fitxers P7

--- PBN/P6/test_modulator.c
+++ PBN/P6/test_modulator.c
@@ -1,11 +1,23 @@
 #include "modulator.h"
 #include <stdbool.h>
 #include <util/delay.h>
+#include "gpio_device.h"
+#include "timer.h"
+static pin_t a;
+
+
+void hol(void){
+	pin_toggle(a);
+}
 
 
 void main(void){
 
+	a= pin_create(&PORTD,6,Output);
+	timer_init();
+	timer_every(TIMER_MS(100),hol);
 	modulator_init();
+	sei();
 	while(1){
 		modulator_set(true);
 		_delay_ms(100);
@@ -13,3 +25,4 @@ void main(void){
 		_delay_ms(100);
 	}
 }
+
---
+++ PBN/P7/src/master.c
@@ -0,0 +1,129 @@
+#include <pbn.h>
+//Creem variables unicament per aquest modul.
+static semaph_t sem;
+static semaph_state_t estat;
+
+int main(void){
+  DDRD=0xFF;
+  uint8_t a;
+  char s[20];
+  control_init();
+  serial_open();
+  while (1){
+    a=readline(s,2);
+    //Comprovem l'ordre forceA. 
+    if ((s[0]=='F') && (s[1]=='A')){
+      estat=control_get_state(StreetB);
+      //Si el semafor B esta en ambar l'A ja es posa verd sol.
+      if (estat == SemApproach){
+	char j[]="COMERR";
+	print(j);
+      }
+      else if (estat == SemOff){
+        char j[]="Off";
+        print (j);
+      }
+      else{
+	//Forcem semafor A a CLEAR.
+	control_force(StreetA);
+	ether_put('F');
+	ether_put('A');
+	ether_put(' ');
+	char j[]="OK";
+	print(j);
+      }
+    }
+    //Comprovem l'ordre forceB.
+    else if ((s[0]=='F') && (s[1]=='B')){
+      estat=control_get_state(StreetA);
+      //Si el semafor B esta en ambar l'A ja es posa verd sol.
+      if (estat == SemApproach){
+	char j[]="COMERR";
+	print(j);
+      }
+      else if (estat == SemOff){
+        char j[]="Off";
+        print (j);
+      }
+      else{
+	//Forcem semafor B a CLEAR.
+	control_force(StreetB);
+	ether_put('F');
+	ether_put('B');
+	ether_put(' ');
+	char j[]="OK";
+	print(j);
+      }
+    }
+    //Comprovem l'ordre stop.
+    else if (s[0]=='H'){
+      //Apagem el semafor.
+      control_off();
+      ether_put('H');
+      ether_put(' ');
+      char j[]="apagat";
+      print(j);
+    }
+    //Comprovem l'ordre start.
+    else if (s[0]=='R'){
+      //Engegem semafor.
+      control_on();
+      ether_put('R');
+      ether_put(' ');
+      char j[]="control_on";
+      print(j);
+    }
+    //Comprovem l'ordre stateA.
+    else if ((s[0]=='?') && (s[1]=='A')){
+      estat=control_get_state(StreetA);
+      //Resposta per a cada estat.
+      if (estat==SemOff){
+	char j[]="El semafor A esta APAGAT";
+	print(j);				
+      }
+      else if (estat==SemClear){
+	char j[]="El semafor A esta VERD";
+	print(j);				
+      }
+      else if (estat==SemApproach){
+	char j[]="El semafor A esta AMBAR";
+	print(j);				
+      }
+      else if (estat==SemStop){
+	char j[]="El semafor A esta VERMELL";
+	print(j);				
+      }
+      else {
+	char j[]="Error:No s'ha trobat l'estat";
+	print(j);
+      }
+    }
+    //Comprovem l'ordre stateB
+    else if ((s[0]=='?') && (s[1]=='B')){
+      estat=control_get_state(StreetB);
+      //Resposta per a cada estat.
+      if (estat==SemOff){
+	char j[]="El semafor B esta APAGAT";
+	print(j);				
+      }
+      else if (estat==SemClear){
+	char j[]="El semafor B esta VERD";
+	print(j);				
+      }
+      else if (estat==SemApproach){
+	char j[]="El semafor B esta AMBAR";
+	print(j);				
+      }
+      else if (estat==SemStop){
+	char j[]="El semafor B esta VERMELL";
+	print(j);				
+      }
+      else {
+	char j[]="Error:No s'ha trobat l'estat";
+	print(j);
+      }
+    }
+  }
+  serial_close();
+  return 0;
+}
