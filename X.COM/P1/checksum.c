e78698fead8b5d23acb0c51fba0684bbc82cb0cd
sergi.carol <sergi.carol@6045123d-4387-4ba5-964e-f94074b1feba>
2014-09-20 14:10:50 +0000

    acabaaaat

--- X.COM/P1/checksum.c
+++ X.COM/P1/checksum.c
@@ -3,52 +3,59 @@
 
 
 typedef struct num{
-	uint8_t a,b; // numero
+	uint8_t a,b;
 } numero;
 
 
 
-uint8_t check_morse(char j[]){
 
-	uint8_t i;
+numero hex_to_num (uint8_t hex){
+	numero num;
+	uint8_t a = (hex>>4);
+	uint8_t b = hex & 0x0F;
+
+	if (a>9 & a<=0x0F) a+=54;
+	else a+=48;
+	if (b>9 & 0x0F) b+=54;
+	else b+=48;
+	num.a=a;
+	num.b=b;
+	return num;
+
+}
+
+
+numero check_morse(char j[]){
+	numero num;
+
 	//en la 'a' guardarem la suma
 	uint16_t a = 0x00;
 	// en la b i ficarem el carry
-	uint8_t b = 0x00;
+	uint8_t b,i = 0x00;
 	while (j[i] != '\0'){
 		a+=j[i];
 		i++; 
 	}
-
 	while (a>0xFF){
 		b = (a >> 8);
 		a&=0xFF;
 		a+=b;
 	}
-	printf("%c\n",a );
 	a=~a;		
-	return a; 
+	num=hex_to_num(a);
+	return num; 
 
-}
-
-numero hex_to_num (uint8_t hex){
-	numero num;
-	num.a = (hex>>4);
-	num.b = hex & 0x0F;
-	return num;
 }
 
 void main (void) {
-	uint8_t a;
 	numero num;
 
 	char j[] = "AAAAA";
 	
-	a = check_morse(j);
-	printf("%c\n",a);
-	num = hex_to_num(a);
-	printf("%d\n", num.a);
-	printf("%d\n", num.b);
+	num = check_morse(j);
+	printf("%c\n", num.a);
+	printf("%c\n", num.b);
 
 
 }
+
