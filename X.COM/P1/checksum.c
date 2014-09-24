31863f80ce2a813a04c71a489d9d3e7c2275efd1
sergi.carol <Sepharon@users.noreply.github.com>
2014-09-24 08:15:45 +0000

    afegides coses

--- X.COM/P1/checksum.c
+++ X.COM/P1/checksum.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <inttypes.h>
-
+#include <stdbool.h>
 
 typedef struct num{
 	uint8_t a,b;
@@ -8,8 +8,21 @@ typedef struct num{
 
 
 
+uint8_t hex_to_byte (numero byte){
+	numero num;
+	uint8_t a,b;
+	a = (byte.a>>4);
+	b = byte.b & 0x0F;
 
-numero hex_to_num (uint8_t hex){
+	if (a>='0' & a<'10') a-=48;
+	else a-=54;
+	if (b>='0' & a<'10') b-=48;
+	else b-=54;
+	return (a|b);
+
+}
+
+numero byte_to_hex (uint8_t hex){
 	numero num;
 	uint8_t a = (hex>>4);
 	uint8_t b = hex & 0x0F;
@@ -25,7 +38,7 @@ numero hex_to_num (uint8_t hex){
 }
 
 
-numero check_morse(char j[]){
+numero checksum(char j[]){
 	numero num;
 
 	//en la 'a' guardarem la suma
@@ -42,17 +55,38 @@ numero check_morse(char j[]){
 		a+=b;
 	}
 	a=~a;		
-	num=hex_to_num(a);
+	num=byte_to_hex(a);
 	return num; 
 
 }
 
+bool check_checksum(char j[]){
+	//en la 'a' guardarem la suma
+	uint16_t a = 0x00;
+	// en la b i ficarem el carry
+	uint8_t b,i = 0x00;
+	while (j[i] != '\0'){
+		a+=j[i];
+		i++; 
+	}
+	while (a>0xFF){
+		b = (a >> 8);
+		a&=0xFF;
+		a+=b;
+	}
+	// Suma normal
+	if (a==0xFF) return true;
+	else return false;
+}
+
+
 void main (void) {
 	numero num;
-
-	char j[] = "AAAAA";
-	
-	num = check_morse(j);
+	char j[64];
+	printf("%s\n","Escriu alguna cosa: " );
+	scanf("%s",j);
+	num = checksum(j);
+	// Fer proba del chech_checksum com??? hex_to_byte ; on afegir?
 	printf("%c\n", num.a);
 	printf("%c\n", num.b);
 
