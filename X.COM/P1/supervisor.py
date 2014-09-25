936b6eba995520e83d183262c759dbb62baa9501
enric.lenard <enric.lenard@6045123d-4387-4ba5-964e-f94074b1feba>
2014-09-25 06:17:36 +0000

    Modificat modul supervisor

---
+++ X.COM/P1/supervisor.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- encoding:utf-8 -*-
+
+from interpret import Interpret
+import serial
+import time
+
+if __name__=="__main__":
+    # Incialitzem el port serie.
+    ser=serial.Serial('/dev/ttyACM0',9600)
+    # Cridem la classe interpet.    
+    I= Interpret()
+    # Afegim les ordres.
+    I._prompt="***"
+    print "Inicializant el Interpret... \n"
+    time.sleep(1.5)
+    I.run()
+    #Ens gurada el contingut despres de ('***')
+    a=raw_input('***')
+    ser.write(a)
+    r=ser.readline()
+    print r
+    ser.close()
