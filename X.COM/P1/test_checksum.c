2f23a8c624db28c73987b2ad1c0ce8988e84e392
enric.lenard <enric.lenard@6045123d-4387-4ba5-964e-f94074b1feba>
2014-10-02 09:18:17 +0000

    PErfeccionat

---
+++ X.COM/P1/test_checksum.c
@@ -0,0 +1,35 @@
+#include <checksum.h>
+
+uint8_t main (void) {
+  numero num;
+  uint8_t i=0,a;
+  bool state;
+  char j[64];
+  char s[]="Escriu alguna cosa";
+  serial_open();
+  print(s);
+  while (serial_can_read());
+  readline(j,64);
+  print(j);
+  num = checksum(j);
+  while(j[i]!='\0'){
+    i++;
+  }
+  j[i++] = num.b;
+  j[i++] = num.a;
+  j[i] = '\0';
+  //-----------------------------------
+  state = check_checksum(j);
+  if (state == true)
+    {
+      char p[]="True";
+      print(p);	
+    }
+  else
+    {
+      char d[]="Fals";
+      print(d);
+    }
+  serial_close();
+  return 0; 
+}
---
+++ X.COM/P1/test_crc.c
@@ -0,0 +1,36 @@
+#include <checksum.h>
+
+uint8_t main (void) {
+  numero num;
+  uint8_t i=0,a;
+  bool state;
+  char j[64];
+  char s[]="Escriu alguna cosa";
+  serial_open();
+  print(s);
+  while (serial_can_read());
+  readline(j,64);
+  print(j);
+  serial_put('\r');
+  num = crc_morse(j);
+  while(j[i]!='\0'){
+    i++;
+  }
+  j[i++] = num.b;
+  j[i++] = num.a;
+  j[i] = '\0';
+  //-----------------------------------
+  state = check_crc(j);
+  if (state == true)
+    {
+      char p[]="True";
+      print(p);	
+    }
+  else
+    {
+      char d[]="Fals";
+      print(d);
+    }
+  serial_close();
+  return 0; 
+} 
