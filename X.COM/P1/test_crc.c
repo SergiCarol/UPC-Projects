37ca9e570aa2dfd97ea807f3ccf7664f68b7c3ee
sergi.carol <Sepharon@users.noreply.github.com>
2014-10-03 15:36:37 +0000

    afegit que faci un loop

--- X.COM/P1/test_crc.c
+++ X.COM/P1/test_crc.c
@@ -5,31 +5,38 @@ uint8_t main (void) {
   uint8_t i=0,a;
   bool state;
   char j[64];
-  char s[]="Escriu alguna cosa";
   serial_open();
-  print(s);
-  while (serial_can_read());
-  readline(j,64);
-  print(j);
-  serial_put('\r');
-  num = crc_morse(j);
-  while(j[i]!='\0'){
-    i++;
-  }
-  j[i++] = num.b;
-  j[i++] = num.a;
-  j[i] = '\0';
-  //-----------------------------------
-  state = check_crc(j);
-  if (state == true)
-    {
-      char p[]="True";
-      print(p);	
+  while(true){
+    char s[]="Escriu alguna cosa";
+
+    print(s);
+    while (serial_can_read());
+    readline(j,64);
+    print(j);
+    num = crc_morse(j);
+    serial_put(num.a);
+    serial_put(num.b);
+    serial_put('\n');
+    serial_put('\r');
+    i=0;
+    while(j[i]!='\0'){
+      i++;
     }
-  else
-    {
-      char d[]="Fals";
-      print(d);
+    j[i++] = num.b;
+    j[i++] = num.a;
+    j[i] = '\0';
+    //-----------------------------------
+    state = check_crc(j);
+    if (state == true)
+      {
+        char p[]="True";
+        print(p);  
+      }
+    else
+      {
+        char d[]="Fals";
+        print(d);
+      }
     }
   serial_close();
   return 0; 
