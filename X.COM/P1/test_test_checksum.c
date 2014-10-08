744af5c22a0fa322d1230cf3f1510f13cbaec7eb
enric.lenard <enric.lenard@6045123d-4387-4ba5-964e-f94074b1feba>
2014-10-08 09:48:30 +0000

    Arreglat

--- X.COM/P1/test_test_checksum.c
+++ X.COM/P1/test_test_checksum.c
@@ -14,18 +14,18 @@ uint8_t main (void) {
     while (serial_can_read());
     readline(j,64);
     print(j);
-    num = checksum(j);
-    serial_put(num.a);
-    serial_put(num.b);
-    serial_put('\n');
-    serial_put('\r');
-    i=0;
-    while(j[i]!='\0'){
-      i++;
-    }
-    j[i++] = num.a;
-    j[i++] = num.b;
-    j[i] = '\0';
+ 
+    state = check_checksum(j);
+    if (state == true)
+      {	
+        char p[]="True";
+        print(p);	
+      }
+    else
+      {
+        char d[]="Fals";
+        print(d);
+      }
   }
   serial_close();
   return 0; 
