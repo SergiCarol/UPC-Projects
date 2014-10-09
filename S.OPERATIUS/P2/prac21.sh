f2c23298dbe3a1d40a1ff633febde1ccd3d7afff
enric.lenard <enric.lenard@6045123d-4387-4ba5-964e-f94074b1feba>
2014-10-09 07:42:13 +0000

    Fet

--- S.OPERATIUS/P2/prac21.sh
+++ S.OPERATIUS/P2/prac21.sh
@@ -3,7 +3,7 @@
 FILE=$(zenity --title="Selecciona un fitxer" --file-selection)
 NAME=$(zenity --title="Escriu una paraula" --entry)
 
-if grep -w $NAME $FILE; then
+if grep $NAME $FILE; then
     echo "Si s'ha trobat la paraula" $NAME
 else
     echo "No s'ha trobat la paraula" $NAME
