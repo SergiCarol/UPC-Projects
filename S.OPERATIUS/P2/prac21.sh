4d52ed2bb1f2b0f501932a8a75f2a165d6eac174
sergi.carol <sergi.carol@6045123d-4387-4ba5-964e-f94074b1feba>
2014-10-13 16:11:58 +0000

    asfd

--- S.OPERATIUS/P2/prac21.sh
+++ S.OPERATIUS/P2/prac21.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-FILE=$(zenity --title="Selecciona un fitxer" --file-selection)
-NAME=$(zenity --title="Escriu una paraula" --entry)
+FILE=$(zenity --title="Selecciona un fitxer" --file-selection );
+NAME=$(zenity --title="Escriu una paraula" --entry);
 
-if grep $NAME $FILE; then
+if grep -q $NAME $FILE; then
     zenity --info --text="S'ha trobat la paraula"
 else
 	zenity --info --text="No s'ha trobat la paraula"    
