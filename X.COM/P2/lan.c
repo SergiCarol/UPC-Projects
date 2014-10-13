6d2126313c852a99fdc08a2d8fde8958f52b4ef0
sergi.carol <Sepharon@users.noreply.github.com>
2014-10-13 10:28:28 +0000

    aoisudh

--- X.COM/P2/lan.c
+++ X.COM/P2/lan.c
@@ -17,7 +17,7 @@ void lan_init(uint8_t no){
 	node_origen = no;
 	ether_init();
 	serial_init();
-	timer_init()
+	timer_init();
 	// acabat
 }
 
@@ -38,7 +38,9 @@ void on_lan_recived(lan_callback_t l){
 	// on_message_recived del ether???
 }
 
-
+void main(void){
+	
+}
 
 
 static block_morse fix(const block_morse b, uint8_t nd){
@@ -46,9 +48,9 @@ static block_morse fix(const block_morse b, uint8_t nd){
 	uint8_t i = 0;
 
 	t[i++]=node_origen;  // node origen ve de la variable global
-	t[i++]=nd // node desti
+	t[i++]=nd; // node desti
 
-	for (;b[i-2]!='\0';i++){
+	for (i;b[i-2]!='\0';i++){
 		t[i]=b[i-2];      // fiquem els valors del block morse dintre la taula
 	}
 	// checksum, fa falta?????
