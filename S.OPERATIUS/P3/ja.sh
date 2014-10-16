041001d9a33c4a902588af72d9326f0ca5e38e6c
sergi.carol <sergi.carol@6045123d-4387-4ba5-964e-f94074b1feba>
2014-10-16 07:03:46 +0000

    ja.sh actualitzat

--- S.OPERATIUS/P3/ja.sh
+++ S.OPERATIUS/P3/ja.sh
@@ -1,1 +1,14 @@
-<
+#!/bin/bash
+
+
+if [ $1='-n' ]; then
+	if [ -d ~/.ja ]; then
+		echo "Esteu segurs que voleu borrar la base de dades? [s/n]"
+		read RESPOSTA
+		[ RESPOSTA='s' ] && rm -f ~/.ja/*
+	else
+	    mkdir ~/.ja
+	fi
+	
+fi
+ 
