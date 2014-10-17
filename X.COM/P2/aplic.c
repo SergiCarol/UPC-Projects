f37aae7428841fa2dbc51a097e6973783964b6f9
sergi.carol <Sepharon@users.noreply.github.com>
2014-10-17 11:30:17 +0000

    aljkshnfd

--- X.COM/P2/aplic.c
+++ X.COM/P2/aplic.c
@@ -1,7 +1,55 @@
 #include "aplic.h"
 
 
-
-void main(void){
-
+uint8_t t_tx[32];
+uint8_t t_rx[32];
+block_morse tx_H = t_tx; // transmissio 
+block_morse rx_H = t_rx; // recepcio
+/*
+void pinta(void){
+	lan_block_get();
 }
+*/
+void main(void){
+	uint8_t a,i=0;
+//	on_lan_recived(pinta);
+	serial_init();
+	sei();
+	serial_put('E');
+	print("Escriu el node d'origen");
+	serial_put('-');
+	serial_put('>');
+	while(serial_can_read()==false);
+	a=serial_get();
+	serial_put(a);
+	lan_init(a);
+	print("\r\nEscriiu el missatge");
+	while(true){
+		while(serial_can_read()==false);
+		a=serial_get();
+		while (a!='\r'){
+			serial_put(a);
+			if (a=='r'){
+				estat=esperant;
+				i=0;
+				print("\n\r Escriu un nou missatge");
+			}
+			else {
+				tx_H[i++]=a;
+				a=serial_get();
+			}
+		}
+		while (lan_can_put()==pendent_enviar);
+		print("\n\rEscriu un node de desti");
+		serial_put('-');
+		serial_put('>');
+		a=serial_get();
+		serial_put(a);
+		tx_H[i]='\0';
+		print(tx_H);
+		i=0;
+		lan_block_put(tx_H,a);
+		
+	}
+}
+
--- X.COM/P2/lan.c
+++ X.COM/P2/lan.c
@@ -5,8 +5,8 @@
 #define MAX_TRY 3 // Numero maxim d'intens
 
 static void fix(const block_morse b, uint8_t nd);
-static bool envia(const block_morse b);
-
+static void envia(void);
+static void comp(void);
 uint8_t t_tx[32];
 uint8_t t_rx[32];
 block_morse tx = t_tx; // transmissio 
@@ -14,13 +14,12 @@ block_morse rx = t_rx; // recepcio
 
 static uint8_t node_origen; // guardem el node d'origen
 static uint8_t intens = 0; // Numero d'intens d'enviar (MAXIM TRES)
-uint8_t estat;
+
 
 void lan_init(uint8_t no){	
   node_origen = no;
   on_message_received(comp);
   ether_init();
-  serial_init();
   timer_init();
 }
 
@@ -32,41 +31,81 @@ void lan_block_put(const block_morse b , uint8_t nd){
   // [no,nd,missatje,checksum]
   estat=pendent_enviar;
   fix(b,nd);
+  	print("\n\rTrama: ");
+  	print(tx); 
   envia();
   intens=0;
   estat=esperant;
 }
 
 void on_lan_recived(lan_callback_t l){
-  funcio=l;
+  // funcio =l
 }
 
 static void fix(const block_morse b, uint8_t nd){
   uint8_t i = 0;
+  numero num;
   tx[i++]=node_origen;  // node origen ve de la variable global
   tx[i++]=nd; // node desti
-  
-  for (i;b[i-2]!='\0';i++){
-    tx[i]=b[i-2];      // fiquem els valors del block morse dintre la taula
+  while (b[i-2]!='\0'){
+  	tx[i]=b[i-2];
+  	i++;
   }
-  // checksum, fa falta?????
+  num=crc_morse(tx);
+  tx[i++]=num.a;
+  tx[i++]=num.b;
+  tx[i]='\0';
 }
 // Faltarien fer mes funcions estatiques per comprovar coses...
 
 static void envia(void){
   //Coprovar 3 cops si pot enviar.
   if (intens < MAX_TRY){
-    if(lan_can_put()){
-      ether_block_put(tx);
+    if(lan_can_put()){// AIxo despres es te que treure (es per mirar que surt)
+      	ether_block_put(tx);
     }
     else{
       intens++;
-      timer_after(TIMER_MS(rand(n)),envia);
+      timer_after(TIMER_MS(rand()),envia);
     }
   }
 }
 
+
+
+void print(char s[]){
+  /* Envia pel port serie tots el elements de la taula s
+     fins que troba un simbol de final de paraula */
+  uint8_t i=0, c;
+  while(s[i]!='\0'){
+    serial_put(s[i]);
+    i++;
+  }
+  serial_put('\r');
+  serial_put('\n');
+}
+
+void readline(char s[],uint8_t m){
+  /* Llegeix un numero de caracters m pel port serie
+     i els fica en la taula s, en el cas de que es llegeixi
+     un caracter no printable deixa de llegir */
   
+  uint8_t i=0,a;
+  a=serial_get();
+  while (m!=i && isgraph(a)){
+      s[i] = a;
+      i++;
+      a=serial_get();
+    
+  }
+  s[i]='\0';
+}
+
+
+
+static void comp(void){
+
+}  
   //comprova{
   //ether_block_get(rx){
   //si es error
