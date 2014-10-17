873a265847b55faf5cefeb3bf18f170d96e2f0a4
sergi.carol <Sepharon@users.noreply.github.com>
2014-10-17 16:10:42 +0000

    Acabat codi falten arreglar bugs i probar-ho (TRANSMICIO TINDRIA QUE FUNCIONAR NP)

--- X.COM/P2/aplic.c
+++ X.COM/P2/aplic.c
@@ -1,30 +1,31 @@
 #include "aplic.h"
 
 
-uint8_t t_tx[32];
-uint8_t t_rx[32];
-block_morse tx_H = t_tx; // transmissio 
-block_morse rx_H = t_rx; // recepcio
-/*
-void pinta(void){
-	lan_block_get();
-}
-*/
+uint8_t ta_tx[32];
+uint8_t ta_rx[32];
+block_morse tx_H = ta_tx; // transmissio 
+block_morse rx_H = ta_rx; // recepcio
+
+static void pinta(void);
+
 void main(void){
-	uint8_t a,i=0;
-//	on_lan_recived(pinta);
+	uint8_t a,i=0,b;
+	on_lan_recived(pinta);
 	serial_init();
 	sei();
-	serial_put('E');
-	print("Escriu el node d'origen");
-	serial_put('-');
-	serial_put('>');
+	print("Escriu el node d'origen i a continuacio el de desti");
 	while(serial_can_read()==false);
 	a=serial_get();
 	serial_put(a);
 	lan_init(a);
-	print("\r\nEscriiu el missatge");
 	while(true){
+		begin:
+		serial_put('-');
+		serial_put('>');
+		b=serial_get();
+		serial_put(b);
+		print("\r\nEscriu el missatge");	
+// -------------------------------------------------------------
 		while(serial_can_read()==false);
 		a=serial_get();
 		while (a!='\r'){
@@ -32,7 +33,10 @@ void main(void){
 			if (a=='r'){
 				estat=esperant;
 				i=0;
-				print("\n\r Escriu un nou missatge");
+				print("\n\rHas reiniciat el programa. Escriu un nou node de desti i escriu un nou missatge");
+				a='\r';
+				break;
+				
 			}
 			else {
 				tx_H[i++]=a;
@@ -40,16 +44,25 @@ void main(void){
 			}
 		}
 		while (lan_can_put()==pendent_enviar);
-		print("\n\rEscriu un node de desti");
-		serial_put('-');
-		serial_put('>');
-		a=serial_get();
-		serial_put(a);
-		tx_H[i]='\0';
-		print(tx_H);
-		i=0;
-		lan_block_put(tx_H,a);
-		
+		if (i>0){
+			tx_H[i]='\0';
+			i=0;
+			lan_block_put(tx_H,b);
+			print("\n\rEscriu un nou node de desti i escriu un nou missatge");
+		}
 	}
+}
+
+
+
+void pinta(void){
+	uint8_t a,i;
+
+	print("\r\nS'ha rebut un missatge de:");
+	a=lan_block_get(rx_H);
+	serial_put(a);
+	print("El missatge rebut es:");
+	print(rx_H);
+
 }
 
--- X.COM/P2/lan.c
+++ X.COM/P2/lan.c
@@ -7,10 +7,10 @@
 static void fix(const block_morse b, uint8_t nd);
 static void envia(void);
 static void comp(void);
-uint8_t t_tx[32];
-uint8_t t_rx[32];
-block_morse tx = t_tx; // transmissio 
-block_morse rx = t_rx; // recepcio
+char t_tx[32];
+char t_rx[32];
+static block_morse tx = t_tx; // transmissio 
+static block_morse rx = t_rx; // recepcio
 
 static uint8_t node_origen; // guardem el node d'origen
 static uint8_t intens = 0; // Numero d'intens d'enviar (MAXIM TRES)
@@ -31,31 +31,45 @@ void lan_block_put(const block_morse b , uint8_t nd){
   // [no,nd,missatje,checksum]
   estat=pendent_enviar;
   fix(b,nd);
-  	print("\n\rTrama: ");
-  	print(tx); 
+  print("\n\rTrama: ");
+  print(tx); 
   envia();
   intens=0;
   estat=esperant;
 }
 
 void on_lan_recived(lan_callback_t l){
-  // funcio =l
+  	lan_callback_t exec_f = l;
 }
 
+
+uint8_t lan_block_get(block_morse b){
+	/* Conclusio meva:
+	Aixo per l'unic que serveix es per passar de la taula morse
+	del lan a la de l'aplicacio */
+	uint8_t i;
+	for (i=0;rx[i+2]!='\0';i++){
+		b[i]=rx[i+2];
+	}
+	b[i]='\0';
+	return rx[0];
+
+
+}
 static void fix(const block_morse b, uint8_t nd){
   uint8_t i = 0;
   numero num;
   tx[i++]=node_origen;  // node origen ve de la variable global
-  tx[i++]=nd; // node desti
-  while (b[i-2]!='\0'){
+  tx[i]=nd; // node desti
+  for (i=2;b[i-2]!='\0';i++){
   	tx[i]=b[i-2];
-  	i++;
   }
   num=crc_morse(tx);
   tx[i++]=num.a;
   tx[i++]=num.b;
   tx[i]='\0';
 }
+
 // Faltarien fer mes funcions estatiques per comprovar coses...
 
 static void envia(void){
@@ -71,6 +85,29 @@ static void envia(void){
   }
 }
 
+
+static void comp(void){
+	/*
+	1- El on_message_recived del init detecta que rebem un missatge
+	2- El on_message_recived crida a la funcio comp ^
+	3- La funcio comp comprova que tot funcioni be i crida a la funcio funcio()
+	4- La funcio funcio() s'hagfa del on_lan_recived
+	5- La funcio on_lan_recived es crida al main de l'applicacio
+	6- La funcio cridada es la funcio pinta del modul aplicacio .c
+	*/
+
+	uint8_t i;
+	// Agafem els blocks
+	ether_block_get(rx);
+	// Comprovem el crc 
+	if (check_crc(rx)){
+	// Comprovem el origen
+		if(rx[1]==node_origen){
+			print("\r\nTot es correcte")
+			funcio();
+		}
+	}
+}  
 
 
 void print(char s[]){
@@ -100,14 +137,3 @@ void readline(char s[],uint8_t m){
   }
   s[i]='\0';
 }
-
-
-
-static void comp(void){
-
-}  
-  //comprova{
-  //ether_block_get(rx){
-  //si es error
-  //si es destinatari
-  //exe.f()}}
