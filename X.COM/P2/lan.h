633403a683c6225d8c2504ef1b4c958c64c59aab
sergi.carol <sergi.carol@6045123d-4387-4ba5-964e-f94074b1feba>
2014-10-17 16:10:42 +0000

    Acabat codi falten arreglar bugs i probar-ho (TRANSMICIO TINDRIA QUE FUNCIONAR NP)

--- X.COM/P2/lan.h
+++ X.COM/P2/lan.h
@@ -11,13 +11,12 @@
 #include "checksum.h"
 
 typedef void (*lan_callback_t)(void);
-typedef void (*funcio)(void);
 
 void lan_init(uint8_t no);
 
 bool lan_can_put(void);
 void lan_block_put(const block_morse b,uint8_t nd);
-
+uint8_t lan_block_get(block_morse b);
 void on_lan_recived(lan_callback_t l);
 
 void print(char s[]);
