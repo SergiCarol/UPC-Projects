5d911eaf7a71af1f7e1065fda33a73f7e5a95d84
sergi.carol <sergi.carol@6045123d-4387-4ba5-964e-f94074b1feba>
2014-10-30 08:53:44 +0000

    tot acabat·

--- S.OPERATIUS/P4/list.c
+++ S.OPERATIUS/P4/list.c
@@ -1,19 +1,26 @@
 #include <sys/types.h>
 #include <dirent.h>
+#include <stdio.h>
 
-struct dirent *readdir(DIR *dirp);
+#define NULL ((void *)0)
 
-int readdir_r(DIR *dirp, struct dirent *entry, struct dirent **result);
+//struct dirent *readdir(DIR *dirp);
+
+//int readdir_r(DIR *dirp, struct dirent *entry, struct dirent **result);
 
 int main(int argc, char *argv[]){
-  struct dirent dir;  
+  struct dirent *dir;
+  //const char *d_name;  
   DIR *fitxer;
   if (argc > 1){
     fitxer=opendir(argv[1]);
-    while(fitxer=!NULL){
-      dir=readdir(fitxer);
-      printf("%c\n",dir.d_name);
-      fitxer=dir;
+    while(dir!=NULL){
+    dir=readdir(fitxer);
+    //d_name= dir->d_name;
+    if (!dir) break;   
+    if (dir -> d_name[0] != '.'){
+      printf("%s\n",dir->d_name);
+      }
     }
   }
 }
--- S.OPERATIUS/P4/mida.c
+++ S.OPERATIUS/P4/mida.c
@@ -1,28 +1,32 @@
-//#include <libc.h>
+#include <sys/types.h>
+#include <dirent.h>
 #include <sys/stat.h>
 #include <stdio.h>
 
+#define NULL ((void *)0)
 
 int main(int argc, char *argv[]) {
-
+	DIR *fitxer;
 	struct stat buffer;
 	int status,size_1,size_2,result=0;
+	struct dirent *dir;
 	if (argc>1){
-		 status=stat(argv[1],&buffer);
-		if (status==0){
-		 		 size_1=buffer.st_size;
-		 		 size_2=(buffer.st_blksize*buffer.st_blocks);
-		 		 result = size_2-size_1;
-		}
-		else {
-			printf("%s\n","No existeix el fitxer");
-			return 1;
+    	fitxer=opendir(argv[1]);
+    	while((dir = readdir(fitxer)) != NULL){  
+    		if (dir -> d_name[0] != '.'){
+				status=stat(dir -> d_name,&buffer);
+				if (status==0){
+		 		 	size_1=buffer.st_size;
+		 		 	size_2=(512*buffer.st_blocks);
+		 		 	result = size_2-size_1;
+					printf("%-20s%8d\n",dir->d_name,result);
+				}
+				else {
+					printf("%s\n","No existeix el fitxer");
+					return 1;
+				}
+			}
 		}
 	}
-
-	printf("%s%d\n","Aquesat es la mida del fitxer: ",size_1);
-	printf("%s%d\n","Aquesat es la mida del block: ",size_2);
-	printf("%s%d\n","Aquesta es al diferencia: ",result);
 	return 0;
-
 }
