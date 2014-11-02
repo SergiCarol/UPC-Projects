68a9a27a7d91f900fd1315202587eb2c76bba31c
sergi.carol <Sepharon@users.noreply.github.com>
2014-11-02 11:20:03 +0000

    codi actualitzat del trans i el recep

---
+++ X.COM/P3/trans.c
@@ -0,0 +1,35 @@
+#include "frame.h"
+
+
+uint8_t ta_tx[32];
+static block_morse tx_H = ta_tx;
+
+int main(void){
+	uint8_t a,i=0;
+	serial_init();
+	sei();
+	frame_init();
+	while(serial_can_read()==false);
+	while(true){
+		for(i=0;a=serial_get()!='\r';i++){
+			serial_put(a);
+			if (a=='r'){
+				i=0;
+				serial_put('\r');
+				serial_put('\n');
+				break;
+			}
+			else{
+				tx_H[i]=a;
+			}
+		}
+		while (frame_can_put()==enviant);
+    	if (i>0){
+      		tx_H[i]='\0';
+      		i=0;
+      		//Enviem el missatge
+      		frame_block_put(tx_H);    
+    	}
+	}
+	return 0;
+}
