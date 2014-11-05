fe42ab437ccf769b67b86684f98b4348560824ef
enric.lenard <enric.lenard@6045123d-4387-4ba5-964e-f94074b1feba>
2014-11-05 10:36:17 +0000

    contirnuar graf

--- X.COM/P3/frame.c
+++ X.COM/P3/frame.c
@@ -24,30 +24,30 @@ static void error(void);
 
 
 void frame_init(void){
-	state = esperant;
-	timer_init();
-  	ether_init();
-  	on_message_received(check);
-  	pin = pin_create(&PORTB,5,Output);
-  	pin_w(pin,false);
-  	if (states_frame == transmissor) on_finished_transmission(timeout);
+  state = esperant;
+  timer_init();
+  ether_init();
+  on_message_received(check);
+  pin = pin_create(&PORTB,5,Output);
+  pin_w(pin,false);
+  if (states_frame == transmissor) on_finished_transmission(timeout);
 }
 
 bool frame_can_put(void){
-	if (ether_can_put()){
-  		if (state = esperant) return true;
-  		else return false;
-  	}
-  	else return false;
+  if (ether_can_put()){
+    if (state = esperant) return true;
+    else return false;
+  }
+  else return false;
 }
 
 
 void frame_block_put(const block_morse b){
-	//[0/1,DADES,CHECKSUM]
-	// Tenim que esperanr-nos fins que haguem acabat de enviar i rebre la confirmacio
-	state = enviant;
-	build(b);
-	send();
+  //[0/1,DADES,CHECKSUM]
+  // Tenim que esperanr-nos fins que haguem acabat de enviar i rebre la confirmacio
+  state = enviant;
+  build(b);
+  send();
 }
 
 void frame_block_get(block_morse b){
@@ -59,109 +59,109 @@ void frame_block_get(block_morse b){
 }
 
 void on_frame_recived(frame_callback_t l){
-	funcio=l;
+  funcio=l;
 }
 
 static void build(block_morse b){
-	uint8_t i;
-	numero num;
-	tx[0]=numeracio_trama;
-	for (i=1;b[i]!='\0';i++){
-		tx[i]=b[i-1];
-	}
-	num=checksum(tx);
-	tx[i++]=num.a;
-	tx[i++]=num.b;
-	tx[i]='\0';
+  uint8_t i;
+  numero num;
+  tx[0]=numeracio_trama;
+  for (i=1;b[i]!='\0';i++){
+    tx[i]=b[i-1];
+  }
+  num=checksum(tx);
+  tx[i++]=num.a;
+  tx[i++]=num.b;
+  tx[i]='\0';
 } 
 
 static void send(void){
-	if (intens < MAX_TRY){
-    	if(ether_can_put()){
-      		// Si el canal no esat ocupat enviem
-      		ether_block_put(tx);
-      		for(uint8_t i=0; i<64;i++) tx[i]='\0';
-    	}
-    	else{
-      		// Si el canal esta ocupat sumem 1 als intents
-      		intens++;
-      		// Esperem un temps aleatori i tornem a provar.
-      		timer_after(TIMER_MS(rand()%((10+1)*1000)),send);
-    	}
-  	}
+  if (intens < MAX_TRY){
+    if(ether_can_put()){
+      // Si el canal no esat ocupat enviem
+      ether_block_put(tx);
+      for(uint8_t i=0; i<64;i++) tx[i]='\0';
+    }
+    else{
+      // Si el canal esta ocupat sumem 1 als intents
+      intens++;
+      // Esperem un temps aleatori i tornem a provar.
+      timer_after(TIMER_MS(rand()%((10+1)*1000)),send);
+    }
+  }
   // En el cas de que no s'hagi pogut enviar encenem el LED
- 	else {
-  		pin_w(pin,true);
-  		state=esperant;
-	}
+  else {
+    pin_w(pin,true);
+    state=esperant;
+  }
 }
 
 static void check(void){
-	ether_block_get(rx);
-	// Comprovem si hem rebut lo que esperavem
-	if (check_checksum(rx)){
-		if (rx[0]==waiting_for)	next();
-		else error();
-	}
-	else pin_w(pin,true);
+  ether_block_get(rx);
+  // Comprovem si hem rebut lo que esperavem
+  if (check_checksum(rx)){
+    if (rx[0]==waiting_for)	next();
+    else error();
+  }
+  else pin_w(pin,true);
 }
 
 void next (void){
-	numero num;
-	if (states_frame==receptor){
-		// Mirem la numeracio de la trama
-		if (rx[0]==0){
-			// Si hem rebut un 0 tenim que enviar un missatge de confirmacio amb una A
-			numeracio_trama = 'A';
-			// I el seguent missatge que rebem te que començar per 1
-			waiting_for = 1;
-		}
-		else {
-			// En cas contrari tenim que enviar una B
-			numeracio_trama = 'B';
-			// I el seguent missatge te que començar per 0
-			waiting_for = 0;
-		}
-		tx[0]=numeracio_trama;
-		num = checksum(tx);
-		tx[1]=num.a;
-		tx[2]=num.b;
-		tx[3]='\0';
-		send();
-		funcio();
-	}
-	// En el cas de que siguem el transmissor
-	else if (states_frame==transmissor) {
-		timer_cancel(timeout_number);
-		// Comprovem si el missatge de confirmacio es una A
-		if (rx[0]=='A'){
-			// Si ho es el seguent missatge te que començar amb un 1
-			numeracio_trama = 1;
-			waiting_for = 'B';
-		}
-		else {
-			numeracio_trama = 0;
-			waiting_for = 'A';
-		}
-		state=esperant
-	}
+  numero num;
+  if (states_frame==receptor){
+    // Mirem la numeracio de la trama
+    if (rx[0]==0){
+      // Si hem rebut un 0 tenim que enviar un missatge de confirmacio amb una A
+      numeracio_trama = 'A';
+      // I el seguent missatge que rebem te que començar per 1
+      waiting_for = 1;
+    }
+    else {
+      // En cas contrari tenim que enviar una B
+      numeracio_trama = 'B';
+      // I el seguent missatge te que començar per 0
+      waiting_for = 0;
+    }
+    tx[0]=numeracio_trama;
+    num = checksum(tx);
+    tx[1]=num.a;
+    tx[2]=num.b;
+    tx[3]='\0';
+    send();
+    funcio();
+  }
+  // En el cas de que siguem el transmissor
+  else if (states_frame==transmissor) {
+    timer_cancel(timeout_number);
+    // Comprovem si el missatge de confirmacio es una A
+    if (rx[0]=='A'){
+      // Si ho es el seguent missatge te que començar amb un 1
+      numeracio_trama = 1;
+      waiting_for = 'B';
+    }
+    else {
+      numeracio_trama = 0;
+      waiting_for = 'A';
+    }
+    state=esperant
+      }
 }
 
 void error(void){
-	if (states_frame==receptor){
-		if (waiting_for==0) {
-			tx[0]='B';
-		}
-		else{
-			tx[0]='A';
-		}
-		num = checksum(tx);
-		tx[1]=num.a;
-		tx[2]=num.b;
-		tx[3]='\0';
-		send();
-	}
-	else frame_block_put(tx);
+  if (states_frame==receptor){
+    if (waiting_for==0) {
+      tx[0]='B';
+    }
+    else{
+      tx[0]='A';
+    }
+    num = checksum(tx);
+    tx[1]=num.a;
+    tx[2]=num.b;
+    tx[3]='\0';
+    send();
+  }
+  else frame_block_put(tx);
 }
 
 
