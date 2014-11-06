91b34cf201d654baa34e00a298898a92caf1764c
sergi.carol <sergi.carol@6045123d-4387-4ba5-964e-f94074b1feba>
2014-11-06 07:48:56 +0000

    actualitzat

--- S.OPERATIUS/P5/parent.c
+++ S.OPERATIUS/P5/parent.c
@@ -1,6 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
 #define POSIX_C_SOURCE_200809L
 
 
 int main (void){
-	
+
+	pid_t pid;
+	pid = fork();
+	pid = fork();
+	pid = fork();
+	switch (pid){
+		case (-1):
+			fprintf(stderr,"Fork failed");
+			exit(1);
+		case (0):
+			printf("%s%d\n","Soc el process ",getpid());
+			exit(0);
+		default:
+			wait(NULL);
+			wait(NULL);
+			wait(NULL);
+			wait(NULL);
+			return 0;
+	}	
 }
