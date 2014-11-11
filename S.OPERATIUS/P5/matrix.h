30f8884ba69f50899cdf58e4ff3c29b7df94e804
sergi.carol <sergi.carol@6045123d-4387-4ba5-964e-f94074b1feba>
2014-11-11 11:26:09 +0000

    afegit matrix

---
+++ S.OPERATIUS/P5/matrix.h
@@ -0,0 +1,24 @@
+#ifndef MATRIX H
+#define MATRIX H
+
+/* dimensio de la matriu */
+#define DIM 8
+
+/* espai que ocupa una matriu */
+#define SIZE (DIM*DIM*sizeof(float))
+
+/* el tipus matriu */
+typedef float (* const matrix) [DIM];
+
+/* escriu pel canal de sortida la matriu 'a' */
+void print matrix(const matrix a);
+
+/* calcula el producte parcial de matrius seguent:
+* r[minf,maxf;minc,maxc] = a[minf,maxf;] x b[;minc,maxc]
+*/
+void prod matrix(const matrix a, const matrix b, matrix r,
+int minf, int maxf, int minc, int maxc);
+
+/* omple 'm' amb el valor 'v' */
+void const matrix(matrix m, float v);
+#endif
