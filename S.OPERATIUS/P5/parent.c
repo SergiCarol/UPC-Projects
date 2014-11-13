f5b2a8da32535bdcd18f34732ede3d9104725bbd
enric.lenard <enric.lenard@6045123d-4387-4ba5-964e-f94074b1feba>
2014-11-13 07:42:52 +0000

    3.6 acabat

--- S.OPERATIUS/P5/parent.c
+++ S.OPERATIUS/P5/parent.c
@@ -9,43 +9,44 @@
 
 int main (void){
 
-	pid_t pid;
-	int i,a,c,e;
-	int t[4];
-	char ch[]="child";
-	char child[8],child2[2];
-	for (i = 0; i < 4; i++){
-		pid = fork();
-		if (pid > 0){	
-			t[i]=pid;
-			wait(&c);
-			if (WIFEXITED(c)){
-				if (WEXITSTATUS(c)==EXIT_FAILURE){
-					for (a=0;a<=i;a++){
-						printf("%s\n","Process matat");
-						kill(t[a],SIGTERM);
-						wait(NULL);
-					}
-					exit(EXIT_FAILURE);
-				}
-			}
-		}	
-		else if (pid == -1){
-			fprintf(stderr,"Fork failed");
-			exit(EXIT_FAILURE);
-		}
-		else if (pid == 0){
-			sprintf(child,"%s%d",ch,i);
-			sprintf(child2,"%d",i);
-			e=execlp("./child",child,"hola",child2,NULL);
-			printf("%d\n",i );
-			if (e!=0){
-				printf("%s\n","Error al cridar el programa");
-				exit(EXIT_FAILURE);
-			}
-			exit(EXIT_SUCCESS);
-		}
-		
-	}
-	exit(EXIT_SUCCESS);
+  pid_t pid;
+  int i,a,c,e;
+  int t[4];
+  char ch[]="child";
+  char child[8],child2[2];
+  for (i = 0; i < 4; i++){
+    pid = fork();
+    if (pid > 0){	
+      t[i]=pid;
+    }	
+    else if (pid == -1){
+      fprintf(stderr,"Fork failed");
+      exit(EXIT_FAILURE);
+    }
+    else if (pid == 0){
+      sprintf(child,"%s%d",ch,i);
+      sprintf(child2,"%d",i);
+      e=execlp("./child",child,"hola",child2,NULL);
+      if (e!=0){
+	printf("%s\n","Error al cridar el programa");
+	exit(EXIT_FAILURE);
+      }
+      exit(EXIT_SUCCESS);
+    } 
+  }
+  for (a=0;a<i;a++){
+  wait(&c);
+  if (WIFEXITED(c)){
+    if (WEXITSTATUS(c)==EXIT_FAILURE){
+      for (a=0;a<i;a++){
+	printf("%s\n","Process matat");
+	kill(t[a],SIGTERM);
+	wait(NULL);
+      }
+      
+      exit(EXIT_FAILURE);
+    }
+  }
+  }
+  exit(EXIT_SUCCESS);
 }
