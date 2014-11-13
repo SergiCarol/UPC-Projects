d0e41f35e9f88345629d792e8cf897f89fc4f333
sergi.carol <sergi.carol@6045123d-4387-4ba5-964e-f94074b1feba>
2014-11-13 08:18:36 +0000

    child 3.8 acabat

--- S.OPERATIUS/P5/child.c
+++ S.OPERATIUS/P5/child.c
@@ -3,14 +3,23 @@
 #include <sys/wait.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <sys/mman.h>
+#include <sys/stat.h>        
+#include <fcntl.h>         
 #define POSIX_C_SOURCE_20080
 
 int main(int argc, char *argv[]){
-	int i;
+	int i,fd;
+	void *addr;
+	struct stat st;
 	if (argc==3){
-		for (i=0;i<3;i++)
-		printf("%s ",argv[i]);
+//		for (i=0;i<3;i++) printf("%s ",argv[i]);
+//		printf("\n");
+		fd = shm_open(argv[1],O_RWR,0);
+		if(fstat(fd,&st)==-1) exit(EXIT_FAILURE);
+		addr=mmap(NULL,st.st_size,PROT_READ | PROT_WRITE,MAP_SHARED,fd,0);
+		close(fd);
 	}
-	printf("\n");
+	
 	exit(EXIT_SUCCESS);
 }
