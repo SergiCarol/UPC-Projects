255df13b77775e6122b067b901d86725b3c3569a
sergi.carol <Sepharon@users.noreply.github.com>
2014-11-13 09:00:42 +0000

    arreglat amtrix

--- S.OPERATIUS/P5/matrix.c
+++ S.OPERATIUS/P5/matrix.c
@@ -4,7 +4,7 @@
 void prod_matrix(const matrix a, const matrix b, matrix r,int minf, int maxf, int minc, int maxc){
   int i,j,k;
   for(i = minf; i < maxf;i++){
-    for(j = minc; i < minc; i++){
+    for(j = minc; j < minc; i++){
       for (k = 0;k < DIM; k++){ 
 	r[j][i]+= a[i][k] * b[k][j];
       }
@@ -15,9 +15,9 @@ void prod_matrix(const matrix a, const matrix b, matrix r,int minf, int maxf, in
 void print_matrix(const matrix a){
   int i,j;
   for (i = 0; i < DIM; i++){
-    for (j = 0; i < DIM; j++){
-      printf("%lf ",a[j][i]);
-      if (i==7) printf("\n");
+    for (j = 0; j < DIM; j++){
+      printf("%lf ",a[i][j]);
+      if (j==7) printf("\n");
     }
   }
   printf("\n");
@@ -26,8 +26,16 @@ void print_matrix(const matrix a){
 void const_matrix(matrix m, float v){
   int i,j;
   for (i = 0; i < DIM; i++){
-    for (j = 0; i < DIM; j++){
-      m[j][i]=v;
+    for (j = 0;j < DIM; j++){
+      m[i][j]=v;
     }
   }
 }
+
+
+/*int main(void){
+	float m[DIM][DIM];
+	const_matrix(m,5);
+	print_matrix(m);
+	return 0;
+}*/
