a97ea83e0d99fec4125561fe0d8f51a0c5ef60d4
enric.lenard <enric.lenard@6045123d-4387-4ba5-964e-f94074b1feba>
2014-11-15 13:00:39 +0000

    Funcions ex3.10

--- S.OPERATIUS/P5/parent.c
+++ S.OPERATIUS/P5/parent.c
@@ -6,76 +6,79 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <fcntl.h>  
+#include <string.h>
 #include "matrix.h"
 #define POSIX_C_SOURCE_200809L
 
 int main (void){
-
+  //int main(int argc, char *argv[]){
   pid_t pid;
   int i,a,c,e,t[4],fd;
   char ch[]="child";
   char child[8],child2[2];
   void *addr;
-  // if(argc==3){
-  fd=shm_open ("nomfit",O_RDWR|O_CREAT,S_IRUSR|S_IWUSR);
-  if (fd==-1) exit(EXIT_FAILURE);
-  if(ftruncate(fd,3*SIZE)==-1)exit(EXIT_FAILURE);
-  addr=mmap(NULL,3*SIZE,PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
-  if(addr==MAP_FAILED) exit(EXIT_FAILURE);
-  // memcpy(addr,"Hola",5);
-  i = close(fd);
-  if (i!=0){
-    printf("%s\n","Close erroni");
-    shm_unlink("nomfit");
-    exit(EXIT_FAILURE);
-  }
-
-    matrix A = addr;
-    matrix B = addr + SIZE;
-    matrix R = addr + 2*SIZE;
-    const_matrix(A,2);
-    const_matrix(B,3);
-    // save_matrix(argv[1],A);
-    //save_matrix(argv[2],B);
-    //save_matrix(argv[3],R);    
-    
-    // }
-    //else exit(EXIT_FAILURE);
-
-  for (i = 0; i < 4; i++){
-    pid = fork();
-    if (pid > 0){	
-      t[i]=pid;
-    }	
-    else if (pid == -1){
-      fprintf(stderr,"Fork failed");
+  //if(argc==3){
+    fd=shm_open ("nomfit",O_RDWR|O_CREAT,S_IRUSR|S_IWUSR);
+    if (fd==-1) exit(EXIT_FAILURE);
+    if(ftruncate(fd,3*SIZE)==-1)exit(EXIT_FAILURE);
+    addr=mmap(NULL,3*SIZE,PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
+    if(addr==MAP_FAILED) exit(EXIT_FAILURE);
+    // memcpy(addr,"Hola",5);
+    i = close(fd);
+    if (i!=0){
+      printf("%s\n","Close erroni");
+      shm_unlink("nomfit");
       exit(EXIT_FAILURE);
     }
-    else if (pid == 0){
-      sprintf(child,"%s%d",ch,i);
-      sprintf(child2,"%d",i);
-      e=execlp("./child",child,"nomfit",child2,NULL);
-      if (e!=0){
-	printf("%s\n","Error al cridar el programa");
+    
+    matrix A = addr;
+    matrix B = addr + SIZE;
+    matrix R = addr + SIZE*2;
+    const_matrix(A,2);
+    const_matrix(B,3);
+    //save_matrix(argv[1],A);
+    //save_matrix(argv[2],B);
+    //save_matrix(argv[3],R);    
+     
+
+    for (i = 0; i < 4; i++){
+      pid = fork();
+      if (pid > 0){	
+	t[i]=pid;
+      }	
+      else if (pid == -1){
+	fprintf(stderr,"Fork failed");
 	exit(EXIT_FAILURE);
       }
-      exit(EXIT_SUCCESS);
-    } 
-  }
-  for (a=0;a<i;a++){
-  wait(&c);
-  if (WIFEXITED(c)){
-    if (WEXITSTATUS(c)==EXIT_FAILURE){
-      for (a=0;a<i;a++){
-	printf("%s\n","Process matat");
-	kill(t[a],SIGTERM);
-	wait(NULL);
+      else if (pid == 0){
+	sprintf(child,"%s%d",ch,i);
+	sprintf(child2,"%d",i);
+	e=execlp("./child",child,"nomfit",child2,NULL);
+	if (e!=0){
+	  printf("%s\n","Error al cridar el programa");
+	  exit(EXIT_FAILURE);
+	}
+	exit(EXIT_SUCCESS);
       }
-      shm_unlink("nomfit");
-      exit(EXIT_FAILURE);     
+    }
+    // } 
+  // else exit(EXIT_FAILURE);
+  
+  for (a=0;a<i;a++){
+    wait(&c);
+    if (WIFEXITED(c)){
+      if (WEXITSTATUS(c)==EXIT_FAILURE){
+	for (a=0;a<i;a++){
+	  printf("%s\n","Process matat");
+	  kill(t[a],SIGTERM);
+	  wait(NULL);
+	}
+	shm_unlink("nomfit");
+	exit(EXIT_FAILURE);     
+      }
     }
   }
-  }
+  
   printf("%s\n","Matriu resultant");
   print_matrix(R);
   //load_matrix(argv[3],R);
