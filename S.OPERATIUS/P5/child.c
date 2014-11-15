77db1bd4891621e643334c2ed6437e11cf1ba484
sergi.carol <sergi.carol@6045123d-4387-4ba5-964e-f94074b1feba>
2014-11-15 20:21:24 +0000

    actualitzat

--- S.OPERATIUS/P5/child.c
+++ S.OPERATIUS/P5/child.c
@@ -18,12 +18,17 @@ int main(int argc, char *argv[]){
     fd = shm_open(argv[1],O_RDWR,0);
     if(fstat(fd,&st)==-1) exit(EXIT_FAILURE);
     addr=mmap(NULL,st.st_size,PROT_READ | PROT_WRITE,MAP_SHARED,fd,0);
-    close(fd);
+    i=close(fd);
+    if (i!=0){
+      printf("%s\n","Close erroni");
+      shm_unlink("nomfit");
+      exit(EXIT_FAILURE);
+    }
+
     matrix AA = addr;
     matrix BB = addr + SIZE;
     matrix RR = addr + SIZE*2;
     if (*argv[2]=='0'){
-      printf("%s\n","Argv 0");
       prod_matrix(AA,BB,RR,0,DIM/2,0,DIM/2);
     }
     else if (*argv[2]=='1'){
