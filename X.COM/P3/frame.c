cfed9a7bb9903ba9e18bbff19d8ce1f2fdf2a51d
sergi.carol <sergi.carol@6045123d-4387-4ba5-964e-f94074b1feba>
2014-11-19 11:02:57 +0000

    alhsd

--- X.COM/P3/frame.c
+++ X.COM/P3/frame.c
@@ -97,7 +97,8 @@ static void send(void){
 }
 
 static void check(void){
-  for(uint8_t i=0;i<32;i++) rx[i]='\0'; 
+  for(uint8_t i=0;i<32;i++) rx[i]='\0';
+  pin_toggle(pin)
   ether_block_get(rx);
   print(rx);
   if (check_crc(rx)){
