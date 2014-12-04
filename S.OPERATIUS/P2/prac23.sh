fa57734aaf53cc879de44b8957bb497388568efe
sergi.carol <sergi.carol@6045123d-4387-4ba5-964e-f94074b1feba>
2014-12-04 07:28:17 +0000

    afegida P6

--- S.OPERATIUS/P2/prac23.sh
+++ S.OPERATIUS/P2/prac23.sh
@@ -4,7 +4,7 @@ FILES=$(find $1 -name "*.py")
 
 for i in $FILES
 do
-    awk '/^class/ { gsub("class ","");gsub(/\(.*/,"");print $0,FILENAME}' $i
+    awk '/^class/ { gsub("class ","");gsub(/\ (.*/,"");print $0,FILENAME}' $i
 done | sort -d
 
 
