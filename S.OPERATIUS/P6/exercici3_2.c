24720f488ebba93e7b1533c89dfdd1493ef1ac70
sergi.carol <Sepharon@users.noreply.github.com>
2015-01-02 14:48:12 +0000

    crec que acabat

---
+++ S.OPERATIUS/P6/exercici3_2.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(void){
+
+  FILE *gnuplotPipe;
+  time_t random;
+
+  gnuplotPipe = popen("gnuplot","w");
+  srand((unsigned)time(&random));
+  
+  if (!gnuplotPipe){
+    fprintf (stderr, "Error\n");
+    exit(EXIT_FAILURE);
+  }
+
+  else {
+    printf ("OK\n");
+    fprintf(gnuplotPipe,"set auto x\n");
+    fprintf(gnuplotPipe,"set yrange [0:30]\n");
+    fprintf(gnuplotPipe,"set style histogram cluster gap 1\n");
+    fprintf(gnuplotPipe,"set style fill transparent solid 0.75 border -3\n");
+    fprintf(gnuplotPipe,"set output 'ex1.png'\n");
+    fprintf(gnuplotPipe,"set boxwidth 0.75\n");
+    fprintf(gnuplotPipe,"set bmargin 3\n");
+    fprintf(gnuplotPipe,"set grid x\n");
+    fprintf(gnuplotPipe,"set grid y\n");
+    fprintf(gnuplotPipe,"set xlabel 'Agrupacions'\n");
+    fprintf(gnuplotPipe,"set ylabel 'Vots (Percentatge)\n");
+    fprintf(gnuplotPipe,"plot '-' u 2:xticlabels(1) w boxes ls 2 notitle\n");
+    fprintf(gnuplotPipe,"Petra 20\n");
+    fprintf(gnuplotPipe,"Pol 3\n");
+    fprintf(gnuplotPipe,"Paula 7\n");
+    fprintf(gnuplotPipe,"Pep 7\n");
+    fprintf(gnuplotPipe,"e\n");
+  }
+  
+  while(true){
+    fflush(gnuplotPipe);
+    sleep(10);
+    fprintf(gnuplotPipe,"replot\n");
+    fprintf(gnuplotPipe,"Petra %d\n",rand() % 30);
+    fprintf(gnuplotPipe,"Pol %d\n",rand() % 30);
+    fprintf(gnuplotPipe,"Paula %d\n",rand() % 30);
+    fprintf(gnuplotPipe,"Pep %d\n",rand() % 30);
+    fprintf(gnuplotPipe,"e\n");
+  }
+  
+  pclose(gnuplotPipe);
+  return 0;
+}
