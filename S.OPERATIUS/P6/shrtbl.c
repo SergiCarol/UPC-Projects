55711d44ff524ac98f3c56d1519366427125ab93
sergi.carol <sergi.carol@6045123d-4387-4ba5-964e-f94074b1feba>
2015-01-16 10:46:26 +0000

    coses facils fetes

--- S.OPERATIUS/P6/shrtbl.c
+++ S.OPERATIUS/P6/shrtbl.c
@@ -1,12 +1,15 @@
 #include "shrtbl.h"
 
 //COMPILAR AMB -pthread
-sem_t w,mutex;  //wait,mutex
+
+sem_t w;  //wait,mutex
+taula data_t;
+
 
 int create_shared_table(void){
   fd=shm_open ("nomfit",O_RDWR|O_CREAT,S_IRUSR|S_IWUSR);
   if (fd==-1) return ERR;
-
+  
   if ((ftruncate(fd,SIZE))==-1){
     shm_unlink("nomfit");
     return ERR;
@@ -51,5 +54,88 @@ void init_table(void){
   int i;
   if (sem_init(w,1,1) == -1) return ERR;
   if (sem_init(mutex,1,1) == -1) return ERR;
-  // apartir d'aqui ja no se xD
+  data_t.max = 0;
+  data_t.read_line = 0;
+			   
+}
+
+
+int add_party(const char id[]){
+  int i;
+  
+  sem_wait(w);
+  // Recorrem la taula
+  for (i = 0; i < data_t.max; i++){
+    // Comrpovem si el partit esta a la taula
+    if (strcmp(data_t.dades[i].partit,id)==0){
+      printf("Aquet partit ja existeix a la taula\n");
+      sem_post(w);
+      return OK;
+    }
+  }
+  // Si no ho esta l'afegim
+  strcpy(data_t.dades[data_t.max].partit,id);
+  (data_t.max)++;
+  sem_post(w);
+  return OK;
+}
+
+int del_party(const char id[]){
+  int i;
+  
+  sem_wait(w);
+  for (i = 0; i < data_t.max; i++){
+    if (strcmp(data_t.dades[i].partit,id)==0){
+      // Canviem el partit de dalt per el que volem canviar
+      data_t.dades[i].partit = data_t.dades[max].partit;
+      data_t.dades[i].vots = data_t.dades[max].vots;
+      // Eliminem el partit de dalt
+      (data_t.max)--;
+      sem_post(w);
+      return OK;
+    }
+  }
+  printf("Aquest partit no existeix\n");
+  sem_post(w);
+  return OK;
+}
+
+void inc_votes(const char party[], int votes){
+  int i;
+  bool a = false;
+  
+  sem_wait(w);
+  for (i = 0; i<data_t.max; i++){
+    if (strcmp(data_t.dades[i].partit,id)==0){
+      data_t.dades[i].vots += votes;
+      a = true;
+      break;
+    }
+  }
+  if (a == false) printf("No s'ha trobat el partit\n");
+  sem_post(w);
+}
+
+int get_votes(const char party[]){
+  int i;
+  
+  sem_wait(w);
+  for(i = 0; i<data_t.max; i++){
+    if (strcmp(data_t.dades[i].partit,id)==0){
+      sem_post(w);
+      return data_t.dades[i].vots;
+    }
+  }
+  printf("Aquet partit no existeix\n");
+  return ERR;
+}
+
+int get_nparties(void){
+  int a;
+  
+  sem_wait(w);
+  a = data_t.max;
+  sem_post(w);
+  
+  return a;
 }
--- S.OPERATIUS/P6/shrtbl.h
+++ S.OPERATIUS/P6/shrtbl.h
@@ -5,12 +5,39 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <semaphore.h>
+#include <string.h>
+
+
 
 #define OK 0
 #define ERR 1
-#define SIZE 10
+#define SIZE 20
+
+typedef struct {
+  char partit [SIZE/2];
+  int vots;
+  // Cada "partit" es una linia
+} partit;
+
+typedef struct{
+  // Linia per la qual estem llegint
+  int read_line;
+  // Linies totals
+  int max;
+  // Numero de linies que podem tenir
+  partit dades[SIZE];
+}taula;
 
 
 
+int create_shared_table(void);
+int remove_shared_table(void);
+int bind_shared_table(void);
+void init_table(void);
+int add_party(const char id[]);
+int del_party(const char id[]);
+void inc_votes(const char party[], int votes);
+int get_votes(const char party[]);
+int get_nparties(void);
 
 #endif
