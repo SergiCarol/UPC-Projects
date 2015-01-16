a965dffa25f134a69c07cbf5be3cdf38e35518cc
sergi.carol <sergi.carol@6045123d-4387-4ba5-964e-f94074b1feba>
2015-01-16 12:56:24 +0000

    asihiygdç

--- S.OPERATIUS/P6/shrtbl.h
+++ S.OPERATIUS/P6/shrtbl.h
@@ -6,29 +6,30 @@
 #include <stdbool.h>
 #include <semaphore.h>
 #include <string.h>
-
+#include <sys/mman.h>
+#include <sys/stat.h>        /* For mode constants */
+#include <fcntl.h> 
 
 
 #define OK 0
 #define ERR 1
-#define SIZE 20
+#define DIM 30
+#define SIZE sizeof(taula)
 
 typedef struct {
-  char partit [SIZE/2];
+  char partit [DIM/2];
   int vots;
   // Cada "partit" es una linia
 } partit;
 
 typedef struct{
-  // Linia per la qual estem llegint
-  int read_line;
-  // Linies totals
   int max;
   // Numero de linies que podem tenir
-  partit dades[SIZE];
+  partit dades[DIM];
+  sem_t *w;  //wait
 }taula;
 
-
+typedef void travapp(const char *const id, int votes, void *const data);
 
 int create_shared_table(void);
 int remove_shared_table(void);
@@ -39,5 +40,7 @@ int del_party(const char id[]);
 void inc_votes(const char party[], int votes);
 int get_votes(const char party[]);
 int get_nparties(void);
+void traverse(travapp *const f, void *const data);
+
 
 #endif
