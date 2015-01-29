64d0d3ff0ca8152c3adaebefae9d8c2379946ae6
sergi.carol <sergi.carol@6045123d-4387-4ba5-964e-f94074b1feba>
2015-01-29 08:44:37 +0000

    afegit del

--- S.OPERATIUS/P6/poll.c
+++ S.OPERATIUS/P6/poll.c
@@ -21,11 +21,13 @@ int main(int argc, char *argv[]){
   case 3 :
     // Si lo que hem escrit es "new"....
     if (strcmp(argv[1],"new") == 0) add_party(argv[2]);
+    else if (strcmp(argv[1],"del") == 0) del_party(argv[2]);
     else printf("Error, parametres incorrectes 1\n");
     break;
   case 4 :
     // Si lo que em escrit es "add"...
     if (strcmp(argv[1],"add") == 0) inc_votes(argv[2],atoi(argv[3]));
+    else if (strcmp(argv[1],"del") == 0) del_party(argv[2]);
     else printf("Error, parametres incorrectes 2\n");
     break;
     // Crec que faria falta un "del" per borrar la taula.....
