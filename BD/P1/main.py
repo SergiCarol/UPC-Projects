3dd0739f758ba6f324e81a478e5f6bac5cc34129
enric.lenard <enric.lenard@6045123d-4387-4ba5-964e-f94074b1feba>
2015-02-20 12:21:42 +0000

    Començat main

--- BD/P1/main.py
+++ BD/P1/main.py
@@ -6,7 +6,14 @@ def consulta(x):
     if llegir_placa(x) == "XXXXXXX":
         print('La plaça '+ str(x) +' esta buida')
     else:
-        print('La plaça ' str(x) 'esta ocupade per' str(llegir_placa(x)))
+        print('La plaça '+ str(x) +' esta ocupada per '+ str(llegir_placa(x)))
 
+def ocupa(p,m):
+    if ocupar_placa(p,m) == True:
+        print('Plaça '+str(p)+' ocupada correctament per '+str(m))
+    else:
+        print('Plaça '+str(p)+' ocupada per '+str(llegir_placa(p)))
+    
 
-consulta(0)
+consulta(1)
+ocupa(2)
