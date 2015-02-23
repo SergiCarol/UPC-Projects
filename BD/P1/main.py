bf14946c7f22beba91184b1114179bbfb3607c67
enric.lenard <enric.lenard@6045123d-4387-4ba5-964e-f94074b1feba>
2015-02-23 10:43:53 +0000

    Acabat main

--- BD/P1/main.py
+++ BD/P1/main.py
@@ -2,24 +2,11 @@
 
 from fitxer import*
 
-def consulta(x):
-    if llegir_placa(x) == "XXXXXXX":
-        print('La plaça '+ str(x) +' esta buida')
-    else:
-        print('La plaça '+ str(x) +' esta ocupada per '+ str(llegir_placa(x)))
-
 def ocupa(p,m):
     if ocupar_placa(p,m) == True:
         print('Plaça '+str(p)+' ocupada correctament per '+str(m))
     else:
         print('Plaça '+str(p)+' ocupada per '+str(llegir_placa(p)))
-
-def sortir(m):
-    if is_in(m) == -1:
-        print('No es traboa dins del parking')
-    else:
-        ocupar_placa(is_in(m),"XXXXXXX")
-        print(str(m)+' ha sortit correctament')
 
 #Aquesta funcio ocupa la primera plaça buida que troba, en cas 
 #de que es pugui ocupar retorna true, en cas contrari false
@@ -30,9 +17,32 @@ def ocupar_first(m):
         print(str(m)+' ha ocupat la plaça '+str(j))
     else:
         print('El parking esta complet')
+
+def sortir(m):
+    if is_in(m) == -1:
+        print('No es traboa dins del parking')
+    else:
+        ocupar_placa(is_in(m),"XXXXXXX")
+        print(str(m)+' ha sortit correctament')
+
+def consulta(x):
+    if llegir_placa(x) == "XXXXXXX":
+        print('La plaça '+ str(x) +' esta buida')
+    else:
+        print('La plaça '+ str(x) +' esta ocupada per '+ str(llegir_placa(x)))
     
 def cerca(m):
     if is_in() == -1:
         print('No es traboa dins del parking')
     else:
         print(str(m)+' esta en la plaça '+str(is_in))
+
+def llistat():
+    if empty_number() == []:
+        print('El parking esta complet')
+    else:
+        print('El parking disposa daquestes places: ')
+        print(list(empty_number()))
+
+
+llista()
