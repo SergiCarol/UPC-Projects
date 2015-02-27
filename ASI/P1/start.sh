b1f0a4b1a9f6727b27a0c760df32a8efc0691d60
enric.lenard <enric.lenard@6045123d-4387-4ba5-964e-f94074b1feba>
2015-02-27 09:39:47 +0000

    frw

--- ASI/P1/start.sh
+++ ASI/P1/start.sh
@@ -2,3 +2,9 @@
 
 sudo kvm derivada.qcow2 -net nic,macaddr=52:54:00:99:34:57 -net tap,script=no &
 sudo kvm derivada2.qcow2 -net nic,macaddr=52:54:00:99:34:58 -net tap,script=no &
+
+sudo brctl addif br0 tap1
+sudo brctl addif br0 tap2
+
+sudo ifconfig tap1 up
+sudo ifconfig tap2 up
