cb29cb6131f043af825ba89c62066bdaec6dea8f
enric.lenard <enric.lenard@6045123d-4387-4ba5-964e-f94074b1feba>
2015-03-05 19:58:00 +0000

    Document.txt fet

--- BD/P1/main2.py
+++ BD/P1/main2.py
@@ -39,7 +39,7 @@ def sortir(m):
     else:
         [a,b,h]=llegir_placa(is_in(m))
         ocupar_placa(is_in(m),"XXXXXXX",'X','X')
-        print('Ha sortit correctament\nmatricula: '+str(a)+'\ncolor: '+str(b.strip('x00'))+'\nmodel: '+str(h.strip('x00'))+)
+        print('Ha sortit correctament\nmatricula: '+str(a)+'\ncolor: '+str(b.strip('x00'))+'\nmodel: '+str(h.strip('x00')))
 
 
 def consulta(p):
