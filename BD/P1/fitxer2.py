d8425a0b74ef7ffc72121f9003ebb9798da30f17
sergi.carol <Sepharon@users.noreply.github.com>
2015-03-05 20:15:33 +0000

    arreglades coses2

--- BD/P1/fitxer2.py
+++ BD/P1/fitxer2.py
@@ -2,28 +2,30 @@
 
 import struct
 
-"""
-Donada una posicio retorna la matricula del cotxe aparcat o
-XXXXXXX en cas de que estigui buida
-"""
 def llegir_placa(p):
+    
+    """
+    Donada una posicio retorna la matricula, el color i la marxa del 
+    cotxe aparcat o XXXXXXX en cas de que estigui buida
+    """
+
     # Obrim el fitxer
     f=open('places.dat','r+')
     # Calculem la posicio que volem mirar
     posicio = p*27
     f.seek(posicio)
     [m,c,t] = struct.unpack('7s10s10s',f.read(27))
-    #c = c.strip('x00')
     f.close()
     return [m,c,t]
     
-    
-"""
-Comprova si la posicio esta ocupada, en el cas de que no ho estigui
-escriu la matricula, si funciona correctament retorna true,
-en cas contrari retorna false
-"""
 def ocupar_placa(p,m,c,t):
+    
+    """
+    Comprova si la posicio esta ocupada, en el cas de que no ho estigui
+    escriu la matricula, el color i la marca, si funciona correctament retorna true,
+    en cas contrari retorna false
+    """
+
     if len(m) == 7 and p < 1000: 
         f=open('places.dat','r+')
         posicio=p*27
@@ -33,18 +35,17 @@ def ocupar_placa(p,m,c,t):
         f.close()
         return True
     else:
-        print len(m)
-        print p
         return False
   
         
-	
-"""
-Aquesta funcio comprova si una matricula es troba dintre 
-el parquing, i retorna la seva posicio, en cas de que no i sigui
-retorna -1
-"""
+
 def is_in(m):
+	
+    """
+    Aquesta funcio comprova si una matricula es troba dintre 
+    el parquing, i retorna la seva posicio, en cas de que no i sigui
+    retorna -1
+    """
     f=open('places.dat','r')
     r = f.read()
     f.close()
@@ -55,14 +56,14 @@ def is_in(m):
         return -1
         
         
-
-"""
-Aquesta funcio busca el numero de plaçes buides que hi ha, 
-les plaçes buides es representen amb la matricula XXXXXXX
-per tan busquem totes les posicions per veure quantes tenen
-espais buits.
-"""
 def empty_number():
+    
+    """
+    Aquesta funcio busca el numero de plaçes buides que hi ha, 
+    les plaçes buides es representen amb la matricula XXXXXXX
+    per tan busquem totes les posicions per veure quantes tenen
+    espais buits.
+    """
     f = open('places.dat','r')
     r= f.read()
     i = 0
@@ -70,25 +71,29 @@ def empty_number():
     
     if is_in("XXXXXXX") < 0:
         return l        
-    
+    print "hola"
     while i < 27027:
         f.seek(i)
         i=r.find("XXXXXXX",i)
-        if i > 0:
-            l.append(i/27)
-            i+=27
+        
+        l.append(i/27)
+        i+=27        
+        print i
     f.close()
         
     return l
 
 
-"""
-Aquesta funcio fica tots els aparcaments buits
-"""
 def buidar():
+    
+    """
+    Aquesta funcio fica tots els aparcaments buits
+    """
     f = open('places.dat','r+')
     for x in range(27026):
         f.seek(x)
         f.write('X')
     f.close()
 
+ocupar_placa(1,"7654321","verd","hola")
+print empty_number()
