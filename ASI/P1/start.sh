8e5c00d8c6e77cc0d5665ed38da23a0d8edcd6ee
enric.lenard <enric.lenard@6045123d-4387-4ba5-964e-f94074b1feba>
2015-03-06 10:16:09 +0000

    gsrehtdjfgkh

--- ASI/P1/start.sh
+++ ASI/P1/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo kvm dns1.qcow2 -net nic,macaddr=52:54:00:99:34:57 -net tap,script=no &
-sudo kvm dns2.qcow2 -net nic,macaddr=52:54:00:99:34:58 -net tap,script=no &
+sudo kvm dns1.qcow2 -net nic,macaddr=52:54:00:99:02:57 -net tap,script=no &
+sudo kvm dns2.qcow2 -net nic,macaddr=52:54:00:99:02:58 -net tap,script=no &
 
 .//home/sergi/projecte-tic/ASI/P1/ips.sh
