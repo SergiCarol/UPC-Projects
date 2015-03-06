436a4b9e3d78483805d63d3068fdc575943ec471
sergi.carol <Sepharon@users.noreply.github.com>
2015-03-06 09:20:34 +0000

    afegit un altre script

--- ASI/P1/start.sh
+++ ASI/P1/start.sh
@@ -3,14 +3,4 @@
 sudo kvm derivada.qcow2 -net nic,macaddr=52:54:00:99:34:57 -net tap,script=no &
 sudo kvm derivada2.qcow2 -net nic,macaddr=52:54:00:99:34:58 -net tap,script=no &
 
-sudo brctl addif br0 tap1
-sudo brctl addif br0 tap2
-
-sudo ifconfig tap1 up
-sudo ifconfig tap2 up
-
-brctl show
-
-sudo sysctl -w net.ipv4.ip_forward=1
-
-sudo iptables -t nat -A POSTROUTING -s 172.20.2.0/16 -o wlan0 -j MASQUERADE
+.//home/sergi/projecte-tic/ASI/P1/ips.sh
