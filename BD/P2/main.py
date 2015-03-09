25368d21060a60efe682fa41aebf48aba80c3f9a
enric.lenard <enric.lenard@6045123d-4387-4ba5-964e-f94074b1feba>
2015-03-09 12:17:33 +0000

    Funcionament correcte sense colisions

--- BD/P2/main.py
+++ BD/P2/main.py
@@ -3,71 +3,62 @@
 from fitxer import *
 from interpret import Interpret
 
-def ocupa(p,m,c,t):
+def ocupa(dni,nom,data):
     """ 
     No es comproven coses dintre ocupar_placa, canviar consulta perque retorni True o false
     o bé fer comrpovacions aqui dintre 
     """
-    if  llegir_placa(int(p)) == ["XXXXXXX","XXXXXXXXXX","XXXXXXXXXX"]:
-        if ocupar_placa(int(p),m,c,t) == True:
-            print('La plaça '+str(p)+' ha estat ocupada correctament per\nmatricula: '+str(m)+'\ncolor: '+str(c)+'\nmodel: '+str(t))
+    if  llegir(dni) == ["XXXXXXXXX","XXXXXXXXXX","XXXX"]:
+        if ocupar(dni,nom,data) == True:
+            print('Shan guardat les dades correctament:\nDNI: '+str(dni)+'\nNOM: '+str(nom)+'\nDATA: '+str(data))
         else:
-            print('Has introduit una plaça o una matricula incorrecte')
+            print('Has introduit una DNI o una DATA incorrectament')
     else:
-        [a,b,h]=llegir_placa(int(p))
-        print('La plaça '+str(p)+' ja està ocupada per\nmatricula: '+str(a)+'\ncolor: '+str(b.strip('x00'))+'\nmodel: '+str(h.strip('x00')))
+        [a,b,h]=llegir(dni)
+        print('Tenim una colisio per\nDNI: '+str(a.strip('x00'))+'\nNOM: '+str(b)+'\nDATA: '+str(h))
 
-def ocupar_first(m,c,t):
-    """
-    Aquesta funcio ocupa la primera plaça buida que troba amb la matricula m.
-    En cas de que es pugui ocupar retorna true, en cas contrari false
-    """
-    p = is_in("XXXXXXX")
-    if p >= 0:
-        ocupar_placa(int(p),m,c,t)
-        print('La plaça '+str(p)+' ha estat ocupada correctament per\nmatricula: '+str(m)+'\ncolor: '+str(c)+'\nmodel: '+str(t))
-    else:
-        print('El parking esta complet')
-
-def sortir(m):
+def sortir(dni):
     """
     Comprovem que la matricula m es troba dins el parking.
     En cas afirmatiu la treiem subtituint el seu valor per 'XXXXXXX'
     """
-    if is_in(m) == -1:
-        print('No es traboa dins del parking')
+    if is_in(dni) == -1:
+        print('No es traboa dins de la taula')
     else:
-        [a,b,h]=llegir_placa(is_in(m))
-        ocupar_placa(is_in(m),"XXXXXXX","XXXXXXXXXX","XXXXXXXXXX")
-        print('Ha sortit correctament\nmatricula: '+str(a)+'\ncolor: '+str(b.strip('x00'))+'\nmodel: '+str(h.strip('x00')))
-
-
+        [a,b,h]=llegir(dni)
+        if subs(is_in(dni)) == True:
+            print('Ha sortit correctament\nDNI: '+str(a)+'\nNOM: '+str(b.strip('x00'))+'\nDATA: '+str(h.strip('x00')))
+        else:
+            print('Hi ha agut un error')
+"""
 def consulta(p):
-    [a,b,h] = llegir_placa(int(p))
+    [a,b,h] = llegir(int(p))
     if a == "XXXXXXX":
         print('La plaça '+ str(p) +' esta buida')
         return True
     else:
         print('La plaça '+ str(p) +' esta ocupada per\nmatricula: '+str(a)+'\ncolor: '+str(b.strip('x00'))+'\nmodel: '+str(h.strip('x00')))
         return False   
-def cerca(m):
-    if len(m)<7:
-        print "La matricula no es correcte"
+"""
+
+def cerca(dni):
+    if len(dni)!=9:
+        print "El DNI no es correcte"
     else:
-        a = is_in(m)
+        a = is_in(dni)
         if a == -1:
-            print('No es traboa dins del parking')
+            print('No es traboa dins la taula')
         else:
-            print('La matricula: '+str(m)+' esta en la plaça '+str(a))
+            print('El DNI: '+str(dni)+' esta en la plaça '+str(a))
 
 def llistat():
     """
     Et retorna una llista amb les places que estan buides.
     """
     if empty_number() == []:
-        print('El parking esta complet')
+        print('La taula esta plena')
     else:
-        print('El parking disposa daquestes places: ')
+        print('La taula disposa daquestes places: ')
         print(list(empty_number()))
 
 def ending():
@@ -84,7 +75,7 @@ def info():
     """
     Et mostra el conjunt de comandes que pots utilitzar en l'interpret.
     """
-    print "Les comanandes són:\n -info\n -exit\n -cerca 'matricula'\n -llistat_buides\n -consulta 'plaça'\n -sortir 'matricula'\n -ocupar_primera 'matricula' 'color' 'marca'\n -ocupar 'plaça' 'matricula' 'color' 'marca'"
+    print "Les comanandes són:\n -info\n -exit\n -reset\n -cerca 'DNI'\n -llistat_buides\n -sortir 'DNI'\n -ocupar 'DNI' 'NOM' 'DATA'"
     print "*Aquells noms entre cometes simples '' indiquen el nom de la variable"
 
 if __name__=="__main__":
@@ -93,12 +84,18 @@ if __name__=="__main__":
     I.set_end(ending)
     I.set_begin(begining)
     I.afegeix_ordre("info",info)
+    I.afegeix_ordre("reset",buidar)
     I.afegeix_ordre("cerca",cerca)
     I.afegeix_ordre("llistat_buides",llistat)
-    I.afegeix_ordre("consulta",consulta)
+    #I.afegeix_ordre("consulta",consulta)
     I.afegeix_ordre("sortir",sortir)
-    I.afegeix_ordre("ocupar_primera",ocupar_first)
+    #I.afegeix_ordre("ocupar_primera",ocupar_first)
     I.afegeix_ordre("ocupar",ocupa)
     I.run()
 
 
+#buidar()
+#ocupa("12345678Q","Pep","1994")
+#sortir("12345678Q")
+#cerca("12345678Q")
+#llistat()
