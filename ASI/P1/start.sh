dbb90c3ef3d759ce21428a4a5aa62399f3709e0c
sergi.carol <sergi.carol@6045123d-4387-4ba5-964e-f94074b1feba>
2015-03-13 09:58:21 +0000

    afegida carpeta 3

--- ASI/P1/start.sh
+++ ASI/P1/start.sh
@@ -2,5 +2,4 @@
 
 sudo kvm dns1.qcow2 -net nic,macaddr=52:54:00:99:02:57 -net tap,script=no &
 sudo kvm dns2.qcow2 -net nic,macaddr=52:54:00:99:02:58 -net tap,script=no &
-
-.//home/sergi/projecte-tic/ASI/P1/ips.sh
+sudo kvm mail1.qcow2 -net nic,macaddr=52:54:00:99:02:59 -net tap,script=no &
