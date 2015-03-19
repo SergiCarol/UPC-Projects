bce8a15132866ddac213ac2693d09e65aec69bf9
enric.lenard <enric.lenard@6045123d-4387-4ba5-964e-f94074b1feba>
2015-03-19 07:34:03 +0000

    makinote

---
+++ BD/P2/fitxer4.py
@@ -0,0 +1,165 @@
+#-*- encoding: utf-8 -*-
+
+import struct
+
+def hash_(a):
+    hashh=0
+    i =1
+    for c in str(a):
+        hashh= (101*hashh + ord(c))*i
+        i+=1
+    return hashh%23023
+
+def llegir(nom):
+    
+    """
+    Donada una posicio retorna la matricula, el color i la marxa del 
+    cotxe aparcat o XXXXXXX en cas de que estigui buida
+    """
+
+    # Obrim el fitxer
+    f=open('persones.dat','r+')
+    # Calculem la posicio que volem mirar
+    posicio = hash_(nom)
+    f.seek(posicio)
+    [nom,dni,data] = struct.unpack('10s9s4s',f.read(23))
+    f.close()
+    return [nom,dni,data]
+
+def posicio(p):
+    
+    """
+    Donada una posicio retorna la matricula, el color i la marxa del 
+    cotxe aparcat o XXXXXXX en cas de que estigui buida
+    """
+
+    # Obrim el fitxer
+    f=open('persones.dat','r+')
+    # Calculem la posicio que volem mirar
+    
+    f.seek(p)
+    [nom,dni,data] = struct.unpack('10s9s4s',f.read(23))
+    f.close()
+    return [nom,dni,data]
+    
+def ocupar(nom,dni,data):
+    
+    """
+    Comprova si la posicio esta ocupada, en el cas de que no ho estigui
+    escriu la matricula, el color i la marca, si funciona correctament retorna true,
+    en cas contrari retorna false
+    """
+
+    if len(dni) == 9 and len(data) == 4: 
+        f=open('persones.dat','r+')
+        posicio=hash_(nom)
+        f.seek(posicio)
+        a = struct.pack('10s9s4s',nom,dni,data)
+        f.write(a)
+        f.close()
+        return True
+    else:
+        return False
+
+def ocupar_seg(nom,dni,data):
+    if len(dni) == 9 and len(data) == 4: 
+        f=open('persones.dat','r+')
+        posicio=hash_(nom)
+        f.seek(posicio+22)
+        [a,b,c] = struct.unpack('10s9s4s',f.read(23))
+        while [a,b,c] != ["XXXXXXXXXX","XXXXXXXXX","XXXX"]:
+            posicio=posicio+22
+            if posicio<=23000:
+                f.seek(posicio)
+                [a,b,c] = struct.unpack('10s9s4s',f.read(23))
+            else:
+                return False
+        a = struct.pack('10s9s4s',nom,dni,data)
+        f.write(a)
+        f.close()
+        return True
+    else:
+        return False
+
+def subs(posicio):
+    if posicio < 23023:
+        f=open('persones.dat','r+')
+        r = f.read()
+        f.seek(posicio)
+        a = struct.pack('10s9s4s',"XXXXXXXXXX","XXXXXXXXX","XXXX")
+        f.write(a)
+        f.close()
+        return True
+    else:
+        return False
+        
+
+def is_in(nom):
+	
+    """
+    Aquesta funcio comprova si una matricula es troba dintre 
+    el parquing, i retorna la seva posicio, en cas de que no i sigui
+    retorna -1
+    
+    Obrir document llegir i trobar posicio on hi ha el nom, f.seek(pos.nom) tornar a buscar, posar tote sles posiscions en una llista.
+    """
+    f=open('persones.dat','r')
+    a=struct.pack('10s',nom) 
+    r=f.read()
+    llista=[]
+    j=0
+    f.close()
+    while True:
+        j=r.find(a,j+1)
+        if j==-1:
+            print llista
+            return llista
+        else:
+            llista.append(j)
+    
+def empty_number():
+    
+    """
+    Aquesta funcio busca el numero de plaçes buides que hi ha, 
+    les plaçes buides es representen amb la matricula XXXXXXX
+    per tan busquem totes les posicions per veure quantes tenen
+    espais buits.
+    """
+    f = open('persones.dat','r')
+    r= f.read()
+    i = 0
+    l = []
+    
+    if is_in("XXXXXXXXXX") < 0:
+        return l        
+    while i < 23023:
+        f.seek(i)
+        i=r.find("XXXXXXXXXX",i)
+        
+        l.append(i/23)
+        i+=23        
+    f.close()
+        
+    return l
+
+
+def buidar():
+    
+    """
+    Aquesta funcio fica tots els aparcaments buits
+    """
+    f = open('persones.dat','r+')
+    for x in range(23023):
+        f.seek(x)
+        f.write('X')
+    f.close()
+    print ('La taula ha sigut buidada')
+
+"""
+ocupar("enric","39397715A","1994")
+ocupar("sergi","39381257J","1994")
+ocupar("Mc","77774525A","1999")
+print llegir("enric")
+print llegir("sergi")
+print llegir("Mc")
+"""
---
+++ BD/P2/main4.py
@@ -0,0 +1,110 @@
+#-*- encoding: utf-8 -*-
+
+from fitxer4 import *
+from interpret import Interpret
+
+def ocupa(nom,dni,data):
+    """ 
+    No es comproven coses dintre ocupar_placa, canviar consulta perque retorni True o false
+    o bé fer comrpovacions aqui dintre 
+    """
+    if  llegir(nom) == ["XXXXXXXXXX","XXXXXXXXX","XXXX"]:
+        if len(nom)>10:
+            print('*La mida del nom ha sigut escursada a 10 caracters')   
+        if ocupar(nom,dni,data) == True:
+            print('Shan guardat les dades correctament:\nNOM: '+str(nom)+'\nDNI: '+str(dni)+'\nDATA: '+str(data))
+        else:
+            print('Has introduit una DNI o una DATA incorrectament')
+    else:
+        [a,b,h]=llegir(nom)
+        print('Tenim una colisio per\nNOM: '+str(a.strip('x00'))+'\nDNI: '+str(b)+'\nDATA: '+str(h))
+        if ocupar_seg(nom,dni,data) == True:
+            print('Shan guardat les dades correctament:\nNOM: '+str(nom)+'\nDNI: '+str(dni)+'\nDATA: '+str(data))
+
+def sortir(nom):
+    """
+    Comprovem que la matricula m es troba dins el parking.
+    En cas afirmatiu la treiem subtituint el seu valor per 'XXXXXXX'
+    """
+    a = is_in(nom)
+    if a == []:
+        print('No es traboa dins de la taula')
+    else:
+        for f in a:
+            [c,b,h]=posicio(f)
+            if subs(f) == True:
+                print('Ha sortit correctament\nNOM: '+str(c)+'\nDNI: '+str(b.strip('x00'))+'\nDATA: '+str(h.strip('x00')))
+            else:
+                print('Hi ha agut un error')
+"""
+def consulta(p):
+    [a,b,h] = llegir(int(p))
+    if a == "XXXXXXX":
+        print('La plaça '+ str(p) +' esta buida')
+        return True
+    else:
+        print('La plaça '+ str(p) +' esta ocupada per\nmatricula: '+str(a)+'\ncolor: '+str(b.strip('x00'))+'\nmodel: '+str(h.strip('x00')))
+        return False   
+"""
+
+def cerca(nom):
+    if len(nom)>10 or len(nom)==0:
+        print "El NOM no es correcte"
+    else:
+        a = is_in(nom)
+        if a == []:
+            print('No es traboa dins la taula')
+        else:
+            for f in a:
+                [c,b,h]=posicio(f)
+                print('NOM: '+str(c.strip('x00'))+'\nDNI: '+str(b)+'\nDATA: '+str(h))
+
+def llistat():
+    """
+    Et retorna una llista amb les places que estan buides.
+    """
+    if empty_number() == []:
+        print('La taula esta plena')
+    else:
+        print('La taula disposa daquestes places: ')
+        print(len(list(empty_number())))
+
+
+def ending():
+    """
+    Funcio que s'executa quan sortim del programa.
+    """
+    print "Gracies per utilitzar el PARKING 'CARLE'"
+
+def begining():
+    print "\n\t\t\t PARKING 'CARLE'"
+    info()
+
+def info():
+    """
+    Et mostra el conjunt de comandes que pots utilitzar en l'interpret.
+    """
+    print "Les comanandes són:\n -info\n -exit\n -reset\n -consulta 'NOM'\n -esborra 'NOM'\n -insereix 'NOM' 'DNI' 'DATA'"
+    print "*Aquells noms entre cometes simples '' indiquen el nom de la variable"
+
+if __name__=="__main__":
+    I= Interpret()
+    I._prompt="=> "
+    I.set_end(ending)
+    I.set_begin(begining)
+    I.afegeix_ordre("info",info)
+    I.afegeix_ordre("reset",buidar)
+    I.afegeix_ordre("consulta",cerca)
+    I.afegeix_ordre("llistat",llistat)
+    #I.afegeix_ordre("consulta",consulta)
+    I.afegeix_ordre("esborra",sortir)
+    #I.afegeix_ordre("ocupar_primera",ocupar_first)
+    I.afegeix_ordre("insereix",ocupa)
+    I.run()
+
+
+#buidar()
+#ocupa("12345678Q","Pep","1994")
+#sortir("12345678Q")
+#cerca("12345678Q")
+#llistat()
