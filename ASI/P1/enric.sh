ea294d9924b21d008c4a510737ec97cd7a27bfb7
enric.lenard <enric.lenard@6045123d-4387-4ba5-964e-f94074b1feba>
2015-03-27 10:28:51 +0000

    ed

--- ASI/P1/enric.sh
+++ ASI/P1/enric.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+sudo gnome-terminal -e "bash -c \"/usr/sbin/openvpn --config client.txt; exec bash\"" &
+
+sleep 15
+
 sudo brctl addif br0 tap0
 sudo ifconfig tap0 up
--- ASI/P1/start.sh
+++ ASI/P1/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo gnome-terminal -e "bash -c \"/usr/sbin/openvpn --config client.txt; exec bash\"" &
+
 sudo kvm dns1.qcow2 -net nic,macaddr=52:54:00:99:02:57 -net tap,script=no &
 sudo kvm dns2.qcow2 -net nic,macaddr=52:54:00:99:02:58 -net tap,script=no &
 sudo kvm mail1.qcow2 -net nic,macaddr=52:54:00:99:02:59 -net tap,script=no &
