6981621e13d5c4d238deb3a6a208d28d9b7100df
sergi.carol <sergi.carol@6045123d-4387-4ba5-964e-f94074b1feba>
2015-04-24 08:44:40 +0000

    afegida carpeta 7

--- ASI/P1/start.sh
+++ ASI/P1/start.sh
@@ -4,7 +4,7 @@
 
 sudo kvm dns1.qcow2 -net nic,macaddr=52:54:00:99:02:57 -net tap,script=no &
 #sudo kvm dns2.qcow2 -net nic,macaddr=52:54:00:99:02:58 -net tap,script=no &
-#sudo kvm mail1.qcow2 -net nic,macaddr=52:54:00:99:02:59 -net tap,script=no &
+sudo kvm mail1.qcow2 -net nic,macaddr=52:54:00:99:02:59 -net tap,script=no &
 #sudo kvm mail2.qcow2 -net nic,macaddr=52:54:00:99:02:60 -net tap,script=no &
 #sudo kvm mail3.qcow2 -net nic,macaddr=52:54:00:99:02:61 -net tap,script=no &
 sudo kvm web.qcow2 -net nic,macaddr=52:54:00:99:02:62 -net tap,script=no &
@@ -15,7 +15,7 @@ sleep 10
 sudo brctl addif br0 tap0
 sudo brctl addif br0 tap1
 sudo brctl addif br0 tap2
-#sudo brctl addif br0 tap3
+sudo brctl addif br0 tap3
 #sudo brctl addif br0 tap4
 #sudo brctl addif br0 tap5
 #sudo brctl addif br0 tap6
@@ -23,7 +23,7 @@ sudo brctl addif br0 tap2
 sudo ifconfig tap0 up
 sudo ifconfig tap1 up
 sudo ifconfig tap2 up
-#sudo ifconfig tap3 up
+sudo ifconfig tap3 up
 #sudo ifconfig tap4 up
 #sudo ifconfig tap5 up
 #sudo ifconfig tap6 up
