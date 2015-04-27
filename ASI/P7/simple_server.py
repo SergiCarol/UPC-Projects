76c7250855934f07f030eaeec001c16ce6cd18aa
sergi.carol <sergi.carol@6045123d-4387-4ba5-964e-f94074b1feba>
2015-04-27 09:20:22 +0000

    afegits servidors python

---
+++ ASI/P7/simple_server.py
@@ -0,0 +1,72 @@
+#-*- encoding: utf-8 -*-
+
+import SimpleHTTPServer as server
+import BaseHTTPServer as base
+import led
+
+class LEDHTTPRequestHandler(base.BaseHTTPRequestHandler):
+        
+        def _head_html(self):
+                self.send_response(200)
+                self.send_header("Content−type2", "text/html")
+		self.end_headers()
+
+        def _head_error_404(self):
+                self.send_error(404,"File not found")
+
+        def do_GET(self):
+                path = self.path
+                host = self.headers['Host']
+                print 'URL: {0} al servidor {1}'.format(path,host)
+                if path == '/led_on':
+                        self._Led_ON()
+                elif path == '/led_off':
+                        self._Led_OFF()
+                else:
+                        self._head_error_404()
+
+        def _Led_ON(self):
+                """
+                Enric et tocara fer el HTML a tu
+                """
+                
+                self._head_html()
+		resposta = """<html>
+<head>
+	<title>S'ha ences el led</title>
+</head>
+<body>
+	<p>Bon dia</p>
+</body>
+</html>
+	"""
+                self.wfile.write(resposta)
+                led.on()
+
+        def _Led_OFF(self):
+
+                """
+                Misma cosa
+                """
+
+                self._head_html()
+		resposta = """<html>
+<head>
+	<title>S'ha ences el led</title>
+</head>
+<body>
+	<p>Bon dia</p>
+</body>
+</html>
+	"""
+                self.wfile.write(resposta)
+                led.off()
+
+
+
+led.setup(18)
+PORT = 8000
+Handler = LEDHTTPRequestHandler
+httpd = base.HTTPServer(("",PORT),Handler)
+print "Serving at port: " + str(PORT)
+httpd.serve_forever()
