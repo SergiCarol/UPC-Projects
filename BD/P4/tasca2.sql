488300a12454962755205143be81240e772d0ac1
sergi.carol <Sepharon@users.noreply.github.com>
2015-05-05 11:57:14 +0000

    apsjdi

--- BD/P4/tasca2.sql
+++ BD/P4/tasca2.sql
@@ -10,12 +10,18 @@ UPDATE clients SET descompte = 1.5 LIMIT 3;
 
 -- 3:
 	
-SELECT codi FROM Productes WHERE descripcio = "Monitor" AND estoc > 0;
+SELECT codi FROM Productes WHERE descripcio like "%Monitor%" AND estoc > 0;
 
 -- 4:
 
-SELECT codiCentre FROM Venedors WHERE edat BETWEEN 21 AND 26 ORDER BY edat
+SELECT codiCentre FROM Venedors WHERE edat BETWEEN 21 AND 26 ORDER BY edat;
 
 -- 5:
 
-SELECT 
+SELECT a.nif,sum(b.unitats*(c.preu-(c.preu*a.descompte))) AS import_amb_descompte FROM clients a, comandes b, productes c WHERE (a.nif=b.nif) AND (b.codiProducte=c.codi) AND year (b.data)=2002 GROUP BY a.nif
+
+-- 6:
+
+SELECT venedors.nom, sum(comandes.unitats) AS tot_unitats_venudes FROM venedors LEFT JOIN comandes ON (venedors.codi=comandes.codiVenedor) GROUP BY venedors.codi limit 10;
+-- 7:
+SELECT productes.codi, productes.descripció FROM productes LEFT JOIN comandes ON (productes.codi=comandes.codiProducte) WHERE comandes.codiProducte IS null
---
+++ BD/P4/tasca3.sql
@@ -0,0 +1,33 @@
+-- 1: 
+
+SELECT emp.emp_no , emp.cognom, dept.dept_no , dept.dnom FROM emp, dept;
+
+-- 2:
+
+SELECT dept.dept_no, dept.dnom, max(emp.salari) FROM dept LEFT JOIN emp ON dept.dept_no = emp.dept_no GROUP BY dept.dept_no, dnom;
+
+-- 3:
+
+SELECT emp.emp_no, emp.cognom, client.client_cod, client.nom FROM emp LEFT JOIN client ON emp.emp_no = client.repr_cod;
+
+-- 4:
+
+SELECT client.client_cod , client.nom , emp.emp_no, emp.cognom FROM client , emp WHERE emp.emp_no = client.repr_cod;
+
+-- 5:
+
+SELECT client.client_cod , client.nom, emp.emp_no, emp.cognom FROM client LEFT JOIN emp WHERE emp.emp_no = client.repr_cod UNION SELECT client.client_cod , client.nom , emp.emp_no , emp.cognom FROM client LEFT OUTER JOIN emp ON  emp.emp_no = client.repr_cod;
+
+-- 6:
+
+SELECT emp.emp_no , emp.cognom, dept.dept_no , dept.dnom FROM emp, dept;
+
+-- 7:
+
+SELECT dept.dept_no, dept.dnom, max(emp.salari) FROM dept LEFT JOIN emp ON dept.dept_no = emp.dept_no GROUP BY dept.dept_no, dnom;
+
+-- 8: no l'he probat perque petava wtf
+
+SELECT e1.dept_no, e1.emp_no, e1.cognom, e1.salari FROM emp e1 WHERE e1.salari >= (SELECT (avg(e2.salari) FROM emp e2 WHERE e2.dept_no = e1.dept_no);
+
+
