17918e5b1ec23a578a5c527fa558731b5fde6e0b
enric.lenard <enric.lenard@6045123d-4387-4ba5-964e-f94074b1feba>
2015-05-07 17:11:57 +0000

    Fet

--- BD/P4/tasca3.sql
+++ BD/P4/tasca3.sql
@@ -26,9 +26,9 @@ SELECT emp.emp_no , emp.cognom, dept.dept_no , dept.dnom FROM emp, dept;
 
 SELECT dept.dept_no, dept.dnom, max(emp.salari) FROM dept LEFT JOIN emp ON dept.dept_no = emp.dept_no GROUP BY dept.dept_no, dnom;
 
--- 8: no l'he probat perque petava wtf
+-- 8:
 
-SELECT e1.dept_no, e1.emp_no, e1.cognom, e1.salari FROM emp e1 WHERE e1.salari >= (SELECT (avg(e2.salari) FROM emp e2 WHERE e2.dept_no = e1.dept_no);
+SELECT e1.dept_no, e1.emp_no, e1.cognom, e1.salari FROM emp e1 WHERE e1.salari <= (SELECT avg(e2.salari) FROM emp e2 WHERE e2.dept_no = e1.dept_no);
 
 -- 9:
 
@@ -56,5 +56,16 @@ SELECT DEPT_NO FROM EMP GROUP BY DEPT_NO ORDER BY AVG(DATA_ALTA) ASC;
 
 -- 15:
 
+SELECT e1.emp_no, e1.cognom, count(comanda.com_num) FROM emp e1, client c1 LEFT JOIN comanda ON (e1.emp_no==c1.repr_cod) AND (comanda.client_cod==c1.client_cod) GROUP BY e1.emp_no ORDER BY e1.cognom;
 
+-- 16:
 
+SELECT emp_no, cognom, count(comanda.com_num) FROM emp, comanda, client WHERE (emp_no==client.repr_cod) AND (comanda.client_cod==client.client_cod) GROUP BY emp_no HAVING com_num>5;
+
+-- 17:
+
+SELECT p1.prod_num, descripcio, com_data, preu_venda FROM producte p1 LEFT JOIN (detall d1 LEFT JOIN comanda c1 ON d1.com_num=c1.com_num) ON p1.prod_num=d1.prod_num WHERE NOT EXISTS (SELECT c2.com_data FROM comanda c2, detall d2, producte p2 WHERE c2.com_num=d2.com_num AND p2.prod_num=d2.prod_num AND p2.prod_num=p1.prod_num AND (c2.com_data>c1.com_data OR (c2.com_data=c1.com_data AND (c2.com_num>c1.com_num OR (c2.com_num=c1.com_num and d2.detall_num>d1.detall_num)))));
+
+--18:
+
+SELECT cl.client_cod, nom, limit_credit, SUM(total) FROM client cl, comanda co WHERE cl.client_cod = co.client_cod AND strftime('%Y', COM_DATA)='1987' GROUP BY cl.client_cod HAVING limit_credit* 50/100 < SUM(total);
