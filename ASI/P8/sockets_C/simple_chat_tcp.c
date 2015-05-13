f475e7115158221857ef1cd0521793598d2279e4
sergi.carol <Sepharon@users.noreply.github.com>
2015-05-13 11:13:04 +0000

    aohsdahds

--- ASI/P8/sockets_C/simple_chat_tcp.c
+++ ASI/P8/sockets_C/simple_chat_tcp.c
@@ -17,27 +17,22 @@ void error(const char *msg){
 }
 
 int dostuff (int sock){
-  struct sockaddr_in si_me, si_other;   
-  int n,s,slen = sizeof(si_other);
-  char buffer[256];
-  bzero(buffer,256);
-  n = recvfrom(sock, buffer, 256, 0, (struct sockaddr *) &si_other, &slen);
-  while (n<0) n = recvfrom(s, buffer, 256, 0, (struct sockaddr *) &si_other, &slen);
-  //printf("Received packet from %d:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
-  printf("Data: %s" , buffer);
-
-  printf("Please enter the message: ");
-  bzero(buffer,256);
-  fgets(buffer,255,stdin);
-
-  if (strcmp("exit\n",buffer) == 0) {
-    printf("%s\n","Tencant Conexio");
-    close(sock);
-    return 1;
-  }    
-  
-  sendto(sock, buffer,n, 0, (struct sockaddr*) &si_other, slen);
-  return 0;
+   int n;
+   char buffer[256];
+      
+   bzero(buffer,256);
+   n = read(sock,buffer,255);
+   if (strcmp("exit\n",buffer) == 0) {
+     printf("%s\n","Tencant Conexio");
+     close(sock);
+     error("Conexio tencada"); 
+   }
+   if (n < 0) error("ERROR reading from socket");
+   printf("Here is the message: %s\n",buffer);
+   printf("Please enter the message: ");
+   bzero(buffer,256);
+   fgets(buffer,255,stdin);   
+   n = write(sock,buffer,strlen(buffer));
 
 }
 int client(char *argv[]){
@@ -47,7 +42,7 @@ int client(char *argv[]){
   char buffer[256];
  
   portno = atoi(argv[2]);
-  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
   if (sockfd < 0)  error("ERROR opening socket");
   
   serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
@@ -88,7 +83,7 @@ int server(char *argv[]){
   struct sockaddr_in serv_addr, cli_addr;
   int n;
 
-  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
   if (sockfd < 0) error("ERROR opening socket");
   bzero((char *) &serv_addr, sizeof(serv_addr));
   portno = atoi(argv[1]);
@@ -97,11 +92,13 @@ int server(char *argv[]){
   serv_addr.sin_port = htons(portno);
   if (bind(sockfd, (struct sockaddr *) &serv_addr,
 	   sizeof(serv_addr)) < 0) 
-    error("ERROR on binding"); 
+    error("ERROR on binding");
+  
+  listen(sockfd,5);
+  clilen = sizeof(cli_addr);
+  newsockfd = accept(sockfd,(struct sockaddr *) &cli_addr, &clilen);
   if (sockfd < 0) error("ERROR on accept");
-  while(true){
-    if (dostuff(sockfd) == 1) break;;
-  }
+  while(true) dostuff(newsockfd);
   printf("%s\n","out");
   close(sockfd);
   return 0; 
