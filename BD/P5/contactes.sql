e9b4a312fd749c3254f5b3bb9f6a0141153747f5
enric.lenard <enric.lenard@6045123d-4387-4ba5-964e-f94074b1feba>
2015-05-13 07:59:08 +0000

    drama total

--- BD/P5/contactes.sql
+++ BD/P5/contactes.sql
@@ -1,14 +1,18 @@
-CREATE TABLE IF NOT EXISTS contacte(nom TEXT PRIMARY KEY, numero INT);
+CREATE TABLE IF NOT EXISTS contacte(
+nom TEXT NOT NULL,
+movil INT (9),
+fix INT (9),
+PRIMARY KEY (nom));
 
-INSERT INTO contacte (nom,numero) VALUES ('Enric',647561339);
-INSERT INTO contacte (nom,numero) VALUES ('Joan',647561338);
-INSERT INTO contacte (nom,numero) VALUES ('Josep',647561337);
-INSERT INTO contacte (nom,numero) VALUES ('Sergi',647561336);
-INSERT INTO contacte (nom,numero) VALUES ('Xavi',647561335);
-INSERT INTO contacte (nom,numero) VALUES ('Pol',647561334);
-INSERT INTO contacte (nom,numero) VALUES ('Dani',647561333);
-INSERT INTO contacte (nom,numero) VALUES ('Oriol',647561332);
-INSERT INTO contacte (nom,numero) VALUES ('Pere',647561331);
-INSERT INTO contacte (nom,numero) VALUES ('Jordi',647561330);
+INSERT INTO contacte (nom,numero) VALUES ('Enric',647561339,938725479);
+INSERT INTO contacte (nom,numero) VALUES ('Joan',647561338,938725478);
+INSERT INTO contacte (nom,numero) VALUES ('Josep',647561337,938725477);
+INSERT INTO contacte (nom,numero) VALUES ('Sergi',647561336,938725476);
+INSERT INTO contacte (nom,numero) VALUES ('Xavi',647561335,938725475);
+INSERT INTO contacte (nom,numero) VALUES ('Pol',647561334,938725474);
+INSERT INTO contacte (nom,numero) VALUES ('Dani',647561333,938725473);
+INSERT INTO contacte (nom,numero) VALUES ('Oriol',647561332,938725472);
+INSERT INTO contacte (nom,numero) VALUES ('Pere',647561331,938725471);
+INSERT INTO contacte (nom,numero) VALUES ('Jordi',647561330,938725470);
 
 
