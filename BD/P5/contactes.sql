3f009b29c342b7758d38eb74e44c1fefcde572af
enric.lenard <enric.lenard@6045123d-4387-4ba5-964e-f94074b1feba>
2015-05-13 06:50:04 +0000

    Afegit P5

---
+++ BD/P5/contactes.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS contacte(nom TEXT PRIMARY KEY, numero INT);
+
+INSERT INTO contacte (nom,numero) VALUES ('Enric',647561339);
+INSERT INTO contacte (nom,numero) VALUES ('Joan',647561338);
+INSERT INTO contacte (nom,numero) VALUES ('Josep',647561337);
+INSERT INTO contacte (nom,numero) VALUES ('Sergi',647561336);
+INSERT INTO contacte (nom,numero) VALUES ('Xavi',647561335);
+INSERT INTO contacte (nom,numero) VALUES ('Pol',647561334);
+INSERT INTO contacte (nom,numero) VALUES ('Dani',647561333);
+INSERT INTO contacte (nom,numero) VALUES ('Oriol',647561332);
+INSERT INTO contacte (nom,numero) VALUES ('Pere',647561331);
+INSERT INTO contacte (nom,numero) VALUES ('Jordi',647561330);
+
+
