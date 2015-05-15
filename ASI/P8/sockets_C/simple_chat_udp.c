4fe91d1181cae117c9f3715d168a25121808b532
enric.lenard <enric.lenard@6045123d-4387-4ba5-964e-f94074b1feba>
2015-05-15 09:06:16 +0000

    v

--- ASI/P8/sockets_C/simple_chat_udp.c
+++ ASI/P8/sockets_C/simple_chat_udp.c
@@ -52,7 +52,7 @@ int client(char *argv[]){
   
   serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
   serv_addr.sin_family = AF_INET;
-  serv_addr.sin_port = htons(10000);
+  serv_addr.sin_port = htons(portno);
 
   if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
     error("ERROR connecting");
