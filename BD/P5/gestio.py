9fda90e057b7aedb6dcaff528f6f161340326835
sergi.carol <Sepharon@users.noreply.github.com>
2015-05-19 11:18:20 +0000

    laihsd

--- BD/P5/gestio.py
+++ BD/P5/gestio.py
@@ -7,21 +7,24 @@ def create_db():
     Crea una bases de dades, aquesta base de dades gestiona els nom i el numero de telefon de cada usuari
     """
     #canviar xarxa_social.db per el nomes que vulguis que tingui la db
-    #db = sqlite3.connect('xarxa_social.db')
+    db = sqlite3.connect('contacte.db')
     cursor = db.cursor()
     #Ficar la Base de Dades aqui
-	#cursor.execute(''' CREATE TABLE IF NOT EXISTS usuaris (email TEXT PRIMARY KEY, nom TEXT, cognom TEXT, poblacio TEXT, dataNaixement DATA, imatge BLOB, pwd TEXT)''')
+    
+    cursor.execute(''' CREATE TABLE IF NOT EXISTS contacte (nom TEXT PRIMARY KEY,fix INT (9))''')
     db.commit()
 
     #AQUI FICAR ELS TRIGGERS
 
-    cursor.execute(''' create trigger R1 BEFORE INSERT 
-    				on BASE DE DADES AQUI
-    				for each row
-    				BEGIN
-    					AQUI FICAR QUERY QUE MIRI SI EL TELEFON QUE S'HA FICATR JA EXISTEIX
-    				END ''')
-
+    cursor.execute(''' 
+    create trigger R1 BEFORE INSERT on contacte
+    FOR EACH ROW
+    WHEN (fix = New.fix)
+    begin
+    DELETE from contacte WHERE Old.fix = fix; 
+    end 
+    ''')
+    db.commit()
     db.close()
 
 
@@ -29,13 +32,10 @@ def insert_values(nom,numero):
     """ 
     Insereix valors a la base de dades.
     """
-    #Aqui tambe tens que canviar el nom de la base de dades per el nom que has ficat a dalt
-    #db = sqlite3.connect('xarxa_social.db')
+    db = sqlite3.connect('contacte.db')
     cursor = db.cursor()
-    #Els try son perque no peti si li afageixes un email que aj existeix (peta pero diu que no es pot inserir)
     try:
-    	#Ficar els inserts aqui
-        #cursor.execute(''' INSERT INTO usuaris (email,nom,cognom,poblacio,DataNaixement,imatge,pwd) VALUES(?,?,?,?,?,?,?) ''', (email,nom,cognom,poblacio,dataNaixement,sqlite3.Binary(readImage(path)),pwd))
+        cursor.execute(''' INSERT INTO contacte (nom,fix) VALUES(?,?) ''', (nom,numero,))
         print "S'ha inserit correctament l'usuari"
         db.commit()
     except sqlite3.IntegrityError:
@@ -45,27 +45,38 @@ def insert_values(nom,numero):
         db.close()
 
 def modificar_values(nom,numero):
-	#db = sqlite3.connect('xarxa_social.db')
-	cursor = db.cursor()
-	try:
-		#cursor.execute()
-		print "S'ha actualitzat l'usuari correctament"
-		db.commit()
+    db = sqlite3.connect('contacte.db')
+    cursor = db.cursor()
+    try:
+        cursor.execute(''' UPDATE contatce SET fix = ? WHERE nom = ?''',(numero,nom))
+        print "S'ha actualitzat l'usuari correctament"
+        db.commit()
     except :
         #print Aqui ficar un print de error si hi ha un ususari repetit
         db.rollback()
     finally:
         db.close()
 
-def delete_value(nom,numero):
-	#db = sqlite3.connect('xarxa_social.db')
-	cursor = db.cursor()
-	try:
-		#cursor.execute()
-		print "S'ha eliminat l'usuari correctament"
-		db.commit()
+def delete_value(nom):
+    db = sqlite3.connect('contacte.db')
+    cursor = db.cursor()
+    try:
+        cursor.execute(''' DELETE from contacte WHERE nom = ?''',(nom,))
+        print "S'ha eliminat l'usuari correctament"
+        db.commit()
     except :
         #print Aqui ficar un print de error si hi ha un ususari repetit
         db.rollback()
     finally:
         db.close()
+
+
+def show_number(nom):
+    
+    db = sqlite3.connect('contacte.db')
+    cursor = db.cursor()
+    cursor.execute(''' SELECT fix FROM contacte WHERE nom = ?''',(nom,))
+    tlf = cursor.fetchone()
+    db.commit()
+    db.close()
+    return tlf[0]
