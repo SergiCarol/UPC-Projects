253c2829711ce1f91cf5face1bdae9ea8db6b35e
enric.lenard <enric.lenard@6045123d-4387-4ba5-964e-f94074b1feba>
2015-05-20 19:11:16 +0000

    Comentada

--- BD/2n parcial/Puntuable2.sql
+++ BD/2n parcial/Puntuable2.sql
@@ -7,7 +7,6 @@ create table usuaris(ID int, nom text, grau int);
 create table amistats(ID1 int, ID2 int);
 create table preferencies(ID1 int, ID2 int);
 
-
 insert into usuaris values (1510, 'Jordan', 9);
 insert into usuaris values (1689, 'Gabriel', 9);
 insert into usuaris values (1381, 'Tiffany', 9);
@@ -63,51 +62,59 @@ tal que automàticament quan s'insereixi un usuari,
 aquest sigui amicPotencial de tot alumne del seu mateix grau
 */
 
-create trigger potencial 
-	after insert on usuaris
-	for each row
-	--when (select count(*) from usuaris) < 4 --(select count(*) from usuaris where (grau = New.grau))) 
-	begin 
-		insert into amicPotencial SELECT New.ID ,ID FROM usuaris WHERE New.grau = grau;
-	end;
-insert into usuaris values (12345,'manasdf',10);
+CREATE TRIGGER POTENCIAL 
+AFTER INSERT ON USUARIS--actua quan detecta una insercio a la taula Usuaris
+FOR EACH ROW
+BEGIN 
+   INSERT INTO AMICPOTENCIAL--inserim el nou usuari a AmicPotencial
+   SELECT NEW.ID ,ID
+   FROM USUARIS WHERE NEW.GRAU = GRAU;--comprovem que siguin del mateix grau
+END;
+
 /*
 2. Escriure un o més triggers per gestionar el grau dels 
 nous usuaris. Si el registre inserit és inferior a 9 o major a 12, 
 cal canviar el valor a NULL. Addicionalment, si el registre 
 inserit té un valor null a grau, cal canviar-lo per 9.
 */
+--Trigger quan valor inferior a 9 o major a 12
+CREATE TRIGGER R2
+AFTER INSERT ON USUARIS
+FOR EACH ROW--per a cada fila
+BEGIN 
+   UPDATE USUARIS SET GRAU = NULL--actualitzem el grau per NULL
+   WHERE (NEW.GRAU < 9 OR NEW.GRAU > 12);--comprovem si compleix condicions
+END;
+--Trigger quan valor inserit te valor NULL
+CREATE TRIGGER R2_2
+AFTER INSERT ON USUARIS 
+FOR EACH ROW
+BEGIN
+   UPDATE USUARIS SET GRAU = 9--actualitzem el grau per 9
+   WHERE NEW.GRAU IS NULL;--NEW.GRAU=> grau introduit
+END;
 
-create trigger R2
-	after insert on usuaris
-	for each row
-	begin 
-		update usuaris SET grau = NULL where (New.grau < 9 OR New.grau > 12);
-	end;
-
-create trigger R2_2
-	after insert on usuaris 
-	for each row
-	begin
-		update usuaris SET grau = 9 WHERE New.grau IS NULL;
-	end;
 /* 
 3. Escriure un o més triggers que mantinguin la relació de 
 simetria en amics. Específicament, si s'elimina (A,B) de amistats, aleshores
 també cal eliminar (B,A). Si s'insereix (A,B), també cal 
 inserir (B,A). No cal mantenir modificacions.
 */
-CREATE TRIGGER BORRAR AFTER DELETE ON AMISTATS
+--Trigger elimina relaicions mutues
+CREATE TRIGGER BORRAR
+AFTER DELETE ON AMISTATS--activa quan detecta eliminacio en Amistats
 FOR EACH ROW
 BEGIN
-DELETE FROM amistats WHERE (OLD.ID1=ID2 AND OLD.ID2=ID1);
+   DELETE FROM AMISTATS--OLD.ID=>usuari de la taula
+   WHERE (OLD.ID1=ID2 AND OLD.ID2=ID1);
 END;
 
-
-CREATE TRIGGER INSEREIX AFTER INSERT ON AMISTATS
+--Trigger que duplica amistat
+CREATE TRIGGER INSEREIX
+AFTER INSERT ON AMISTATS
 FOR EACH ROW
 BEGIN
-INSERT INTO amistats VALUES (NEW.ID2, NEW.ID1);
+   INSERT INTO amistats VALUES (NEW.ID2, NEW.ID1);--inserim amistat girada
 END;
 
 /*
@@ -115,25 +122,28 @@ END;
 aquests es graduin, és a dir, quan el grau superi el valor 12
 */
 
-create trigger R3
-	after update of grau on usuaris
-	for each row
-	when (New.grau > 12)
-	begin 
-		delete from usuaris where Old.ID = ID;
-	end;
+CREATE TRIGGER R3
+AFTER UPDATE OF GRAU ON USUARIS
+FOR EACH ROW
+WHEN (NEW.GRAU > 12)--actua quan es compleixi GRAU>12
+BEGIN 
+   DELETE FROM USUARIS
+   WHERE OLD.ID = ID;--ID=> identificador introduit
+END;
 
 /*
 5. Escriure un trigger tal que quan un usuari incrementi un grau, 
 també ho facin els seus amics.
 */
 
-create trigger increment 
-	after update of grau on usuaris
-	for each row 
-	begin
-		update usuaris SET grau = grau+1 WHERE ID IN (SELECT ID2 FROM amistats WHERE ID1 = New.ID);
-	end;
+CREATE TRIGGER INCREMENT 
+AFTER UPDATE OF GRAU ON USUARIS--actua al detectar modificacio de grau
+FOR EACH ROW 
+BEGIN
+   UPDATE USUARIS SET GRAU = GRAU+1 --Incrementem el valor del grau
+   WHERE ID IN (SELECT ID2 FROM AMISTATS
+		WHERE ID1 = NEW.ID);--en funcio del usuari introduit
+END;
 
 /*
 6. Escriure un trigger que forci el següent comportament: 
@@ -144,10 +154,13 @@ i (C,B). Comproveu que el trigger únicament funciona quan és
 preferit (B) no el (A)
 */
 --DELETE (B=OLD.ID2,C=ID2)
-CREATE TRIGGER PREF AFTER UPDATE OF ID2 ON preferencies
+
+CREATE TRIGGER PREF
+AFTER UPDATE OF ID2 ON PREFERENCIES
 FOR EACH ROW
-WHEN EXISTS (SELECT * FROM amistats WHERE (ID2=NEW.ID2 AND ID1=OLD.ID2)) 
-BEGIN
-DELETE FROM amistats WHERE (ID1=OLD.ID2 AND ID2=ID2);
-DELETE FROM amistats WHERE (ID2=ID2 AND OLD.ID1=ID2);
+WHEN EXISTS (SELECT * FROM AMISTATS --Tenim en compte si són amics
+	     WHERE (ID2=NEW.ID2 AND ID1=OLD.ID2)) 
+BEGIN--Borrem les amistats simetricament 
+   DELETE FROM AMISTATS WHERE (ID1=OLD.ID2 AND ID2=ID2);
+   DELETE FROM AMISTATS WHERE (ID2=ID2 AND OLD.ID1=ID2);
 END;
