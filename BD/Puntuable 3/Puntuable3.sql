703de2c1b91bd1430cd6d23990eee33e565fa5d4
enric.lenard <enric.lenard@6045123d-4387-4ba5-964e-f94074b1feba>
2015-06-01 07:45:59 +0000

    Afegit Puntuable 3

---
+++ BD/Puntuable 3/Puntuable3.sql
@@ -0,0 +1,33 @@
+create table productes
+(idProducte char(9),
+nom char (20),
+mida char (20),
+preu integer check(preu>0),
+primary key (idProducte),
+unique (nom, mida));
+
+create table domicilis
+(numero char(9),
+carrer char (20),
+numCarrer integer check(numCarrer>0),
+pis char(2),
+porta char(2),
+primary key (numero));
+
+create table comandes
+(numComanda integer check(numComanda>0),
+instantFeta integer not null check(instantFeta>0),
+instantServida integer check(instantServida>0),
+numTelf char(9),
+import integer check(import>0),
+primary key (numComanda),
+foreing key (numTelf) references domicilis,
+check(instantServida>instantFeta));
+
+create table liniesComandes
+(numComanda integer,
+idProducte char(9),
+quantitat integer check(quantitat>0),
+primary key(numComanda,idProducte),
+foreing key (numComanda) references comandes,
+foreing key (idProducte) reference productes);
